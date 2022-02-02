92bf8331b48e01304b12269c63cd1707764e3d28
iago <iago.pferreiravr@gmail.com>
2022-02-02 19:00:52 -0300

    feat(file_process): Req3.2
    Implementa a verificação se o arquivo já existe na fila criada no arquivo 'queue'
    
    Caso o arquivo já esteja na fila ele é ignorado, caso não esteja é adicionado à fila

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -4,14 +4,15 @@ from ting_file_management.file_management import txt_importer
 
 
 def process(path_file, instance):
-    instance.enqueue(path_file)
-    info = txt_importer(path_file)
-    answer_dict = {
-        "nome_do_arquivo": path_file,
-        "qtd_linhas": len(info),
-        "linhas_do_arquivo": info,
-    }
-    sys.stdout.write(str(answer_dict))
+    if path_file not in instance._list:
+        instance.enqueue(path_file)
+        info = txt_importer(path_file)
+        answer_dict = {
+            "nome_do_arquivo": path_file,
+            "qtd_linhas": len(info),
+            "linhas_do_arquivo": info,
+        }
+        sys.stdout.write(str(answer_dict))
 
 
 def remove(instance):
