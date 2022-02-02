9acecdbd4737865529a625a41700594d56a5c8b3
iago <iago.pferreiravr@gmail.com>
2022-02-02 18:55:05 -0300

    feat(file_management): Req2.2
    Implementando a verificação da extensão do arquivo no método 'txt_importer'
    
    Se o arquivo não for '.txt' um erro é exibido no terminal

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,3 +1,8 @@
+import sys
+
+
 def txt_importer(path_file):
+    if not path_file.endswith('.txt'):
+        return sys.stderr.write("Formato inválido\n")
     with open(path_file, "r") as file:
         return list(file.read().splitlines())
