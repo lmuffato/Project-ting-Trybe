7ce7f6667efa79dee0dfd1592700f3bad8b75ea0
iago <iago.pferreiravr@gmail.com>
2022-02-02 18:59:36 -0300

    feat(file_process): Req3.1
    Implementando o método 'process'
    
    Esse método retorna informações sobre o arquivo que está sendo lido pelo método 'txt_importer'

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,17 @@
+import sys
+
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    instance.enqueue(path_file)
+    info = txt_importer(path_file)
+    answer_dict = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(info),
+        "linhas_do_arquivo": info,
+    }
+    sys.stdout.write(str(answer_dict))
 
 
 def remove(instance):
