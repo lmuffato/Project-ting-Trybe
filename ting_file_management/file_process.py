f0242d8e2df076b0b6c5437cc5aa410fb2151eba
iago <iago.pferreiravr@gmail.com>
2022-02-02 19:03:28 -0300

    feat(file_process): Req4.1
    Implementando o método 'remove'
    
    Esse método remove um arquivo da fila e retorna o nome do arquivo removido

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -16,7 +16,8 @@ def process(path_file, instance):
 
 
 def remove(instance):
-    """Aqui irá sua implementação"""
+    file_name = instance.dequeue()
+    sys.stdout.write(f"Arquivo {file_name} removido com sucesso\n")
 
 
 def file_metadata(instance, position):
