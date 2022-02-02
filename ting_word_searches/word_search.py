3de9e1c1f927ea5e1626c75ebad694ce36b2582c
iago <iago.pferreiravr@gmail.com>
2022-02-02 19:14:46 -0300

    feat(word_search): Req6.1
    Implementando o método 'exists_word'
    
    Esse método verifica se uma palavra específica existe no arquivo e retorna informações sobre sua ocorrência

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,19 @@
+from ting_file_management.file_management import txt_importer
+
+
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    return_list = list()
+    for file in instance._list:
+        found_word = list()
+        index = 1
+        for line in txt_importer(file):
+            if word.lower() in line.lower():
+                found_word.append({"linha": index})
+            index += 1
+        return_list.append(
+            {"palavra": word, "arquivo": file, "ocorrencias": found_word}
+        )
+    return return_list
 
 
 def search_by_word(word, instance):
