1d89280e2dcdba2ab017f4ec46396f3996257c35
iago <iago.pferreiravr@gmail.com>
2022-02-02 18:56:30 -0300

    feat(file_management): Req2.3
    Implementando a verificação da existência do arquivo, no método 'txt_importer'
    
    Caso não exista o arquivo ou o caminho para o arquivo esteja errado, um erro é exibido no terminal

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -4,5 +4,8 @@ import sys
 def txt_importer(path_file):
     if not path_file.endswith('.txt'):
         return sys.stderr.write("Formato inválido\n")
-    with open(path_file, "r") as file:
-        return list(file.read().splitlines())
+    try:
+        with open(path_file, "r") as file:
+            return list(file.read().splitlines())
+    except FileNotFoundError:
+        sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
