2cc89ebf97644fcb8bb850299e5b492278116683
iago <iago.pferreiravr@gmail.com>
2022-02-02 19:32:14 -0300

    refactor(word_search): Criando os métodos 'search' e 'search_type'
    Esses métodos são responsáveis, respectivamente, por fazer a busca de uma palavra e retornar o conteúdo da linha ou não
    
    Assim as partes iguais dos métodos 'exists_word' e 'search_by_word' são delegadas para esse métodos

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,33 +1,33 @@
 from ting_file_management.file_management import txt_importer
 
 
-def exists_word(word, instance):
+def search_type(word, file, type):
+    found_word = list()
+    index = 1
+    for line in txt_importer(file):
+        if word.lower() in line.lower():
+            if type == "line":
+                found_word.append({"linha": index})
+            if type == "content":
+                found_word.append({"linha": index, "conteudo": line})
+        index += 1
+    return found_word
+
+
+def search(word, instance, type):
     return_list = list()
     for file in instance._list:
-        found_word = list()
-        index = 1
-        for line in txt_importer(file):
-            if word.lower() in line.lower():
-                found_word.append({"linha": index})
-            index += 1
+        found_word = search_type(word, file, type)
         if len(found_word) != 0:
             return_list.append(
                 {"palavra": word, "arquivo": file, "ocorrencias": found_word}
             )
     return return_list
+
+
+def exists_word(word, instance):
+    return search(word, instance, 'line')
 
 
 def search_by_word(word, instance):
-    return_list = list()
-    for file in instance._list:
-        found_word = list()
-        index = 1
-        for line in txt_importer(file):
-            if word.lower() in line.lower():
-                found_word.append({"linha": index, "conteudo": line})
-            index += 1
-        if len(found_word) != 0:
-            return_list.append(
-                {"palavra": word, "arquivo": file, "ocorrencias": found_word}
-            )
-    return return_list
+    return search(word, instance, "content")
