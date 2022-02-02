4fb7ca8b8f5503c7a766ca1078f077c0096f24be
mallmann02 <leomallmann020302@gmail.com>
2022-02-02 17:09:40 -0300

    feat: text file importer

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,12 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if not path_file.endswith('.txt'):
+      print('Formato inválido', file=sys.stderr)
+    try:
+      with open(path_file, 'r', encoding='utf8') as file:
+        content = file.readlines()
+        return [line.rstrip('\n') for line in content]
+    except FileNotFoundError:
+      print(f'Arquivo {path_file} não encontrado', file=sys.stderr)
