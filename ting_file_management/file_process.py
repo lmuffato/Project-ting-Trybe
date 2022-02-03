7127bbdcf0539b8a51fa7a9af711257cc645e923
Carolina Vasconcelos <carolinavpinheiro@gmail.com>
2022-02-03 19:18:22 -0300

    feat: requisito 5

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -4,14 +4,8 @@ from ting_file_management.file_management import txt_importer
 
 def process(path_file, instance):
     if path_file not in instance.list_ting:
-        list_file = txt_importer(path_file)
         instance.enqueue(path_file)
-        data = {
-            "nome_do_arquivo": path_file,
-            "qtd_linhas": len(list_file),
-            "linhas_do_arquivo": list_file
-        }
-        return sys.stdout.write(f"{data}")
+        return format_data(path_file)
 
 
 def remove(instance):
@@ -23,4 +17,17 @@ def remove(instance):
 
 
 def file_metadata(instance, position):
-    """Aqui irá sua implementação"""
+    if len(instance) < position:
+        return sys.stderr.write("Posição inválida")
+    else:
+        return format_data(instance.list_ting[position])
+
+
+def format_data(path_file):
+    list_file = txt_importer(path_file)
+    data = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(list_file),
+        "linhas_do_arquivo": list_file
+    }
+    sys.stdout.write(f"{data}")
