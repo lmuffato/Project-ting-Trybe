d9281227ac67dff10296987a100a0b27090c56e6
mallmann02 <leomallmann020302@gmail.com>
2022-02-03 14:10:54 -0300

    feat: search word ocurrences
    returns line number and content if it matches

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,3 +1,6 @@
+import re
+
+
 def exists_word(word, instance):
     found_ocurrences = []
     for file in instance.queue:
@@ -7,7 +10,7 @@ def exists_word(word, instance):
             "ocorrencias": []
         })
         for index, line in enumerate(file['linhas_do_arquivo']):
-            if word in line:
+            if re.search(word, line, re.IGNORECASE):
                 found_ocurrences[-1]['ocorrencias'].append({
                     "linha": index + 1
                 })
@@ -16,4 +19,18 @@ def exists_word(word, instance):
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    found_ocurrences = []
+    for file in instance.queue:
+        found_ocurrences.append({
+            "palavra": word,
+            "arquivo": file['nome_do_arquivo'],
+            "ocorrencias": []
+        })
+        for index, line in enumerate(file['linhas_do_arquivo']):
+            if re.search(word, line, re.IGNORECASE):
+                found_ocurrences[-1]['ocorrencias'].append({
+                    "linha": index + 1,
+                    "conteudo": line
+                })
+
+    return list(filter(lambda x: len(x['ocorrencias']) > 0, found_ocurrences))
