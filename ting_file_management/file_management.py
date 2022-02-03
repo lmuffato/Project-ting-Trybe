a428080741a25abdb6c631cee0f5ae596bdec0ce
Marcus Hüsemann <mchusemann@gmail.com>
2022-02-03 17:07:20 -0300

    feat: Implementa funçao file_management

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,13 @@
+# os path source: https://pythonspot.com/read-file/
+import os.path
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if os.path.isfile(path_file):
+        if path_file.endswith('.txt'):
+            with open(path_file) as file:
+                content = file.read().splitlines()
+                return content
+        return sys.stderr.write("Formato inválido\n")
+    return sys.stderr.write(f'Arquivo {path_file} não encontrado\n')
