db00a225c3d15ac173fd0c9ad81aa51acd6f8b6d
Rafael Geronimo <git_config_user_email>
2022-02-03 16:53:03 -0300

    feat: req2.3 | `txt_importer` function | validate import with nonexistend file

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -3,10 +3,13 @@ import sys
 
 def txt_importer(path_file):
     if 'txt' in path_file:
-        data = open(path_file, 'r')
-        content = []
-        for line in data:
-            content.append(line.rstrip('\n'))
-        return content
+        try:
+            data = open(path_file, 'r')
+            content = []
+            for line in data:
+                content.append(line.rstrip('\n'))
+            return content
+        except FileNotFoundError:
+            print(f'Arquivo {path_file} não encontrado', file=sys.stderr)
     else:
         print('Formato inválido', file=sys.stderr)
