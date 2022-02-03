54538b72b6a7c98ead77ed4e7b37a78f7d82749d
Ana Ventura <anaventura1811@gmail.com>
2022-02-03 16:22:17 -0300

    feat: requisito 2 e referências

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,18 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if path_file.endswith('.txt'):
+        try:
+            txt_file = open(path_file, "r")
+            return txt_file.read().splitlines()
+        except FileNotFoundError:
+            # print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
+            sys.stderr.write(f'Arquivo {path_file} não encontrado\n')
+    else:
+        # print("Formato inválido", file=sys.stderr)
+        sys.stderr.write('Formato inválido\n')
+
+# Source:
+# https://docs.python.org/3.0/library/sys.html#module-sys
+# https://stackoverflow.com/questions/5574702/how-to-print-to-stderr-in-python
