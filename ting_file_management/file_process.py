306753aba9ca2cfd380b0eb2dd546c8f77d7030c
Hernanne Domiciano <78621976+hernannegp@users.noreply.github.com>
2022-02-03 17:38:08 -0300

    feat: req03 and 04

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,10 +1,26 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    if path_file not in instance.list:
+        instance.enqueue(path_file)
+        data = txt_importer(path_file)
+        data_format = {
+            "nome_do_arquivo": path_file,
+            "qtd_linhas": len(data),
+            "linhas_do_arquivo": data
+        }
+        return sys.stdout.write(str(data_format))
 
 
 def remove(instance):
-    """Aqui irá sua implementação"""
+    try:
+        delFile = instance.dequeue()
+        return sys.stdout.write(f'Arquivo {delFile} removido com sucesso\n')
+    except IndexError:
+        return print('Não há elementos')
 
 
 def file_metadata(instance, position):
-    """Aqui irá sua implementação"""
+    """lalala"""
