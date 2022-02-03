a507aaef6897335b72b198b5dba0da95d3c7432f
Fernando Castello Branco <fernandocastellob@gmail.com>
2022-02-03 16:20:31 -0300

    FEAT:Req02

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,12 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if not path_file.endswith("txt"):
+        return sys.stderr.write("Formato inválido\n")
+    try:
+        with open(path_file, mode="r") as file:
+            text = file.read().split("\n")
+        return text
+    except FileNotFoundError:
+        return sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
