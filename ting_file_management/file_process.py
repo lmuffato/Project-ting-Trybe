7c5f0774d5b8d8512310191f1e088f5635219f59
mallmann02 <leomallmann020302@gmail.com>
2022-02-02 23:30:55 -0300

    feat: enqueue file data and print it on stdout

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,19 @@
+from .file_management import txt_importer
+import sys
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    for position in range(len(instance)):
+        if instance.search(position)['nome_do_arquivo'] == path_file:
+            return None
+    file_content = txt_importer(path_file)
+    file_data = {
+      "nome_do_arquivo": path_file,
+      "qtd_linhas": len(file_content),
+      "linhas_do_arquivo": file_content
+    }
+    instance.enqueue(file_data)
+    print(file_data, file=sys.stdout)
 
 
 def remove(instance):
