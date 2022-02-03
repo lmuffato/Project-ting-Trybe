5315c24221a337b617db63a0c01f250ce7e5d29d
maricegalla <maricegalla@gmail.com>
2022-02-03 14:54:29 -0300

    feat: :sparkles: Requirement 1

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -1,15 +1,17 @@
 class Queue:
     def __init__(self):
-        """Inicialize sua estrutura aqui"""
+        self.items = []
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        return len(self.items)
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        self.items.append(value)
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        return self.items.pop(0)
 
     def search(self, index):
-        """Aqui irá sua implementação"""
+        if index < 0:
+            raise IndexError
+        return self.items[index]
