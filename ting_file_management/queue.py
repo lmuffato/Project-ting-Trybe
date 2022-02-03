61fcc3391e6bafcefe18115e36d5537190b42837
Felipe Flores <felipefloresvendas@gmail.com>
2022-02-03 20:45:45 -0300

    feat: requirement 1

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -1,16 +1,23 @@
+# https://www.tutorialspoint.com/python/python_deque.htm#:~:text=A%20double%2Dended%20queue%2C%20or,be%20invoked%20directly%20with%20arguments.
+# https://www.youtube.com/watch?v=RHb-8hXs3HE
+
+from collections import deque
+
+
 class Queue:
     def __init__(self):
-        """Inicialize sua estrutura aqui"""
-        pass
+        self.head_value = deque()
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        return len(self.head_value)
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        return self.head_value.append(value)
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        return self.head_value.popleft()
 
     def search(self, index):
-        """Aqui irá sua implementação"""
+        if (0 > index or index > self.__len__()):
+            raise IndexError
+        return self.head_value[index]
