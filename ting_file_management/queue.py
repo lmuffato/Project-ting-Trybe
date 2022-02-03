34578ec97205bcf55c275a8a90d522e8be33399c
nascjoao <oi@joaonasc.dev>
2022-02-03 17:32:19 -0300

    feat: implement queue class

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -1,15 +1,18 @@
 class Queue:
     def __init__(self):
-        """Inicialize sua estrutura aqui"""
+        self.list = list()
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        return len(self.list)
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        self.list.append(value)
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        return self.list.pop(0)
 
     def search(self, index):
-        """Aqui irá sua implementação"""
+        value = self.list[index]
+        if not value or index < 0:
+            raise IndexError
+        return value
