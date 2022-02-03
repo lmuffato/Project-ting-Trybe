c1a438f9242669d7d827e6f6cbd7c3ee97d27c2e
Marcus Hüsemann <mchusemann@gmail.com>
2022-02-03 20:04:00 -0300

    feat: implementa funçao que processa arquivos e salva formatado na fila

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,10 +1,26 @@
+from ting_file_management import file_management
+
+
+def exists_data(instance):
+    for index in range(len(instance)-1):
+        return instance.search(index)
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    file_lines = file_management.txt_importer(path_file)
+    file_data = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(file_lines),
+        "linhas_do_arquivo": file_lines
+    }
+    if (instance.is_empty() or exists_data(instance)):
+        instance.enqueue(file_data)
+        print(file_data)
 
 
 def remove(instance):
-    """Aqui irá sua implementação"""
+    pass
 
 
 def file_metadata(instance, position):
-    """Aqui irá sua implementação"""
+    pass
