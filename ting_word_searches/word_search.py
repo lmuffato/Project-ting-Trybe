fec41076c9ccdf8f0ac152ba00e594078183a86f
Ana Ventura <anaventura1811@gmail.com>
2022-02-03 19:16:02 -0300

    feat: implementa método exists_word

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,24 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    word_ocurrences_list = list()
+    for txt_file in instance.queue:
+        data = {
+            "palavra": word,
+            "arquivo": txt_file["nome_do_arquivo"],
+            "ocorrencias": []
+        }
+        word_ocurrences_list.append(data)
+        for index, line in enumerate(txt_file["linhas_do_arquivo"]):
+            current_line = index + 1
+            word_lowercase = word.casefold()
+            line_lowercase = line.casefold()
+            if word_lowercase in line_lowercase:
+                word_ocurrences_list[index]["ocorrencias"].append({
+                    "linha": current_line
+                })
+                if len(word_ocurrences_list[index]["ocorrencias"]) > 0:
+                    return word_ocurrences_list
+        return list()
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    pass
