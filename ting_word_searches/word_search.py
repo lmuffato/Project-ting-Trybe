be0cab9a39a1ba3094f0cd65a18283f4a935b4a1
Gui-lira <gui.lira.martins@gmail.com>
2022-02-03 18:21:30 -0300

    ultimo

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -16,4 +16,20 @@ def exists_word(word, instance):
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    return_list = []
+    for file in instance.queue:
+        lines = file['linhas_do_arquivo']
+        add_dict = {
+            'palavra': word,
+            'arquivo': file['nome_do_arquivo'],
+            'ocorrencias': [],
+        }
+        for index in range(len(lines)):
+            if word.lower() in lines[index].lower():
+                add_dict['ocorrencias'].append({
+                    'linha': index + 1,
+                    'conteudo': lines[index],
+                    })
+        if len(add_dict['ocorrencias']) > 0:
+            return_list.append(add_dict)
+    return return_list
