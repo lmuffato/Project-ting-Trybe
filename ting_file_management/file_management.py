258b9978678c8ad9f082a844bbe3f59cd051b6be
nascjoao <oi@joaonasc.dev>
2022-02-03 18:59:39 -0300

    feat: implement txt importer

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,16 @@
+import csv
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if not path_file.endswith('.txt'):
+        return sys.stderr.write('Formato inválido\n')
+    try:
+        with open(path_file) as file:
+            reader = csv.reader(file, delimiter="\n")
+            rows = list()
+            for row in reader:
+                rows.append(row[0])
+            return rows
+    except FileNotFoundError:
+        return sys.stderr.write(f'Arquivo {path_file} não encontrado\n')
