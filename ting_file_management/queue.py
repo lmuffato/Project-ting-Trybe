6daa1b05f0bd5b8c40a5925f1495af673be14415
Ana Ventura <anaventura1811@gmail.com>
2022-02-03 15:51:23 -0300

    feat: inicia projeto, req. 1

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -1,15 +1,22 @@
+from collections import deque
+
+
 class Queue:
     def __init__(self):
-        """Inicialize sua estrutura aqui"""
+        self.queue = deque()
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        self.__length = len(self.queue)
+        return self.__length
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        self.queue.append(value)
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        self.queue.popleft()
 
     def search(self, index):
-        """Aqui irá sua implementação"""
+        if index >= 0:
+            return self.queue[index]
+        else:
+            raise IndexError
