ffd5cfbbc7bf32127c02a3d47a8aee00d6d2d6dd
Viegas <mvrdgs@gmail.com>
2022-02-03 16:40:05 -0300

    requisito 2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,16 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    file_name, file_format = path_file.split(".")
+
+    try:
+        if (file_format != "txt"):
+            print("Formato inválido", file=sys.stderr)
+
+        with open(path_file) as file:
+            content = file.read()
+            return content.splitlines()
+
+    except FileNotFoundError:
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
