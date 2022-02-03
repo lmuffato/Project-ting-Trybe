0508dafb1aedc7ccc8fdabf9fe489204dbf707ce
nascjoao <oi@joaonasc.dev>
2022-02-03 19:10:09 -0300

    feat: implement process in file_process

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,16 @@
+from ting_file_management.file_management import txt_importer
+import sys
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    if path_file not in instance.list:
+        instance.enqueue(path_file)
+        txt = txt_importer(path_file)
+        sys.stdout.write(str({
+            "nome_do_arquivo": path_file,
+            "qtd_linhas": len(txt),
+            "linhas_do_arquivo": txt
+        }))
 
 
 def remove(instance):
