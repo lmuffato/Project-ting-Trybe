136e44f431fcd298c0eae460cbab88cdbdb14e71
Hernanne Domiciano <78621976+hernannegp@users.noreply.github.com>
2022-02-03 16:10:44 -0300

    feat: req02

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,11 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementaçãoa"""
+    if not path_file.endswith('.txt'):
+        return sys.stderr.write('Formato inválido\n')
+    try:
+        with open(path_file, 'r') as file:
+            return list(file.read().splitlines())
+    except FileNotFoundError:
+        sys.stderr.write(f'Arquivo {path_file} não encontrado\n')
