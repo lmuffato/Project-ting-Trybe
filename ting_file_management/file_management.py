ff2a77e7630916bc81a226c2ab58d7814c8c0150
Gui-lira <gui.lira.martins@gmail.com>
2022-02-03 14:53:05 -0300

    2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,14 @@
+import sys
+import os
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if not path_file.endswith('.txt'):
+        print('Formato inválido', file=sys.stderr)
+        return None
+    if not os.path.exists(path_file):
+        print(f'Arquivo {path_file} não encontrado', file=sys.stderr)
+        return None
+    with open(path_file, 'r') as f:
+        lines = f.read().splitlines()
+        return lines
