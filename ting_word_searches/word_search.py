376f22cc5ead83b8b6a0f6c8d926c0c45bd719ec
mallmann02 <leomallmann020302@gmail.com>
2022-02-03 11:19:18 -0300

    feat: search word ocurrences
    search in every enqueued file data lines that contain the specified word

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,18 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    found_ocurrences = []
+    for file in instance.queue:
+        found_ocurrences.append({
+            "palavra": word,
+            "arquivo": file['nome_do_arquivo'],
+            "ocorrencias": []
+        })
+        for index, line in enumerate(file['linhas_do_arquivo']):
+            if word in line:
+                found_ocurrences[-1]['ocorrencias'].append({
+                    "linha": index + 1
+                })
+
+    return list(filter(lambda x: len(x['ocorrencias']) > 0, found_ocurrences))
 
 
 def search_by_word(word, instance):
