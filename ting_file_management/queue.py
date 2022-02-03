6dd1b297a299660e6cebe243c522fe8de85d1cd2
Gabriel <g.pereira.a.m@gmail.com>
2022-02-03 18:31:39 -0300

    feat(Req 1) - :sparkle: Requisito 1 feito

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -1,15 +1,56 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
 class Queue:
     def __init__(self):
-        """Inicialize sua estrutura aqui"""
+        self.head_value = None
+        self.__length = 0
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        return self.__length
+
+    def insert_first(self, value):
+        first_value = Node(value)
+        first_value.next = self.head_value
+        self.head_value = first_value
+        self.__length += 1
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        last_value = Node(value)
+        current_value = self.head_value
+
+        if current_value is None:
+            return self.insert_first(value)
+
+        while current_value.next:
+            current_value = current_value.next
+        current_value.next = last_value
+        self.__length += 1
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        value_to_be_removed = self.head_value
+
+        if value_to_be_removed:
+            self.head_value = self.head_value.next
+            value_to_be_removed.next = None
+            self.__length -= 1
+        return value_to_be_removed.value
 
     def search(self, index):
-        """Aqui irá sua implementação"""
+        value_returned = None
+        value_to_be_returned = self.head_value
+
+        if 0 > index or index > self.__length or self.__length == 0:
+            raise IndexError
+
+        if value_to_be_returned:
+            while index > 0 and value_to_be_returned.next:
+                value_to_be_returned = value_to_be_returned.next
+                index -= 1
+
+            if value_to_be_returned:
+                value_returned = Node(value_to_be_returned.value)
+        return value_returned.value
