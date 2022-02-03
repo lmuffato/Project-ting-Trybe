cd95bacb394db4d78c7961e6cb9104247bd0d927
giovanni-maldonado <giovannitadeu08@gmail.com>
2022-02-03 17:38:34 -0300

    feat: requisito 3

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,17 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    if path_file not in instance.list:
+        instance.enqueue(path_file)
+        file_data = txt_importer(path_file)
+        result_dict = {
+            "nome_do_arquivo": path_file,
+            "qtd_linhas": len(file_data),
+            "linhas_do_arquivo": file_data,
+        }
+        sys.stdout.write(str(result_dict))
 
 
 def remove(instance):
