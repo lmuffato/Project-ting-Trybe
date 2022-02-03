d151a875d788e11c6737c2c9a00a8b644e57bf19
Viegas <mvrdgs@gmail.com>
2022-02-03 17:34:44 -0300

    requisitos 3 e 4

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,9 +1,32 @@
+import sys
+from .file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    for index in range(len(instance)):
+        if path_file in instance.search(index)["nome_do_arquivo"]:
+            return None
+
+    file = txt_importer(path_file)
+    data = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(file),
+        "linhas_do_arquivo": file,
+    }
+
+    instance.enqueue(data)
+    print(data, file=sys.stdout)
 
 
 def remove(instance):
-    """Aqui irá sua implementação"""
+    if len(instance) <= 0:
+        print("Não há elementos", file=sys.stdout)
+    else:
+        item_removed = instance.dequeue()
+        print(
+            f"Arquivo {item_removed['nome_do_arquivo']} removido com sucesso",
+            file=sys.stdout,
+        )
 
 
 def file_metadata(instance, position):
