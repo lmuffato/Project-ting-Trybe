c0d646322dd20fd914db15308325cf3a7ee468d3
MarcioFujita <hanker78@gmail.com>
2022-02-03 17:42:07 -0300

    feat: Requisito 2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,16 @@
+import os.path
+import sys
+
+
 def txt_importer(path_file):
     """Aqui irá sua implementação"""
+    if not os.path.isfile(path_file):
+        return sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
+
+    if not path_file.endswith(".txt"):
+        return sys.stderr.write("Formato inválido\n")
+
+    with open(path_file) as file:
+        response = file.read().splitlines()
+        # https://www.tutorialspoint.com/python3/string_splitlines.htm
+        return response
