2e08bd30bc8f2db7de273930d14f08e544026f5e
Gui-lira <gui.lira.martins@gmail.com>
2022-02-03 16:00:26 -0300

    3

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,20 @@
+from sys import stdout
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    lines = txt_importer(path_file)
+    print_dict = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(lines),
+        "linhas_do_arquivo": lines,
+    }
+    mapped = list(map(lambda x: x["nome_do_arquivo"], instance.queue))
+    if path_file in mapped:
+        return None
+    instance.enqueue(print_dict)
+    for key in print_dict.keys():
+        print(f'{key}:{print_dict[key]}', file=stdout)
 
 
 def remove(instance):
