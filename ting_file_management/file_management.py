3728c2eb8967e1a78f812d1e4bc288a4834a22dc
Marcelofcdantas <65692996+Marcelofcd@users.noreply.github.com>
2022-02-02 21:07:27 -0300

    iniciando o projeto

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,10 @@
+import sys
+
+# sys visto no repositório do Rafael Medeiros
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    try:
+        if not path_file.endswith('txt'):
+            print('Formato inválido', file=sys.stderr)
+    except FileNotFoundError:
+        print(f'Arquivo {path_file} não encontrado', file=sys.stderr)
