88d53394d075b64a7ebc091f8616529e77fd80b7
Rafael Geronimo <git_config_user_email>
2022-02-04 09:22:47 -0300

    feat: req3.1 | `process` function | validate process function successfully

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,15 @@
+# from ting_file_management.queue import Queue
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    file = txt_importer(path_file)
+    data = {
+        'nome_do_arquivo': path_file,
+        'qtd_linhas': len(file),
+        'linhas_do_arquivo': file
+    }
+    print(data)
 
 
 def remove(instance):
@@ -8,3 +18,6 @@ def remove(instance):
 
 def file_metadata(instance, position):
     """Aqui irá sua implementação"""
+
+# project = Queue()
+# process('statics/arquivo_teste.txt', project)
