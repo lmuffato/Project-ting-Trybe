4c67d07893d442fb037c443aaaf81839b8d7be3e
Michael <michaelpetterson06.com.br>
2022-02-04 19:37:23 -0300

    req 03

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,17 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    if instance.__len__() < 1:
+        text = txt_importer(path_file)
+        file_info = {
+            "nome_do_arquivo": path_file,
+            "qtd_linhas": len(text),
+            "linhas_do_arquivo": text,
+        }
+        instance.enqueue(file_info)
+        sys.stdout.write(f"{file_info}")
 
 
 def remove(instance):
