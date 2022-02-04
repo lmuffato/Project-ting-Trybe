9e722f159f76eaebaf7b089f6044d541b8960d4d
AndreBarroso <andre_barroso17@hotmail.com>
2022-02-04 19:11:03 -0300

    feat: requisito 2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,12 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if not path_file.endswith(".txt"):
+        return sys.stderr.write('Formato inválido\n')
+    data = open(path_file, "r")
+    if(not data):
+        return sys.stderr.write(f'Arquivo {path_file} não encontrado\n')
+
+    text = data.read().splitlines()
+    return text
