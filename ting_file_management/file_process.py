3a8461c8b26e4c6533dfa8c65072e6ddd87dcace
Fernando Castello Branco <fernandocastellob@gmail.com>
2022-02-04 12:21:50 -0300

    FEAT:Req03

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,10 +1,25 @@
+from ting_file_management.file_management import txt_importer
+import sys
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    data = txt_importer(path_file)
+    data_details = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(data),
+        "linhas_do_arquivo": data,
+    }
+    for el in instance.data:
+        if path_file == el["nome_do_arquivo"]:
+            return False
+
+    instance.enqueue(data_details)
+    return sys.stdout.write(f"{data_details}\n")
 
 
 def remove(instance):
-    """Aqui irá sua implementação"""
+    pass
 
 
 def file_metadata(instance, position):
-    """Aqui irá sua implementação"""
+    pass
