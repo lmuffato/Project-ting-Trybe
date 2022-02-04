170b606c5b5bd7d9a034cdd073c804abb5dec031
Renan Oliveira <oliveira.renan.rg@gmail.com>
2022-02-04 15:32:17 -0300

    start: sisth requirement

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,18 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    occurences = list()
+    for file in instance.get():
+        matches = list()
+        for index, line in enumerate(file['linhas_do_arquivo']):
+            if line.lower().count(word.lower()) > 0:
+                matches.append({'linha': index + 1})
+        if len(matches) > 0:
+            occurence = {
+                'palavra': word,
+                'arquivo': file,
+                'ocorrencias': matches
+            }
+            occurences.append(occurence)
+    return occurences
 
 
 def search_by_word(word, instance):
