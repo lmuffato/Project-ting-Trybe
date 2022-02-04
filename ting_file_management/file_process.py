d38ce9ccb48ad8bae8e9bdad7fc187d8683c4d7c
Felipe Flores <felipefloresvendas@gmail.com>
2022-02-03 21:25:05 -0300

    feat: requirement 3

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,17 @@
+from ting_file_management.file_management import txt_importer
+import sys
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    if path_file not in instance.head_value:
+        instance.enqueue(path_file)
+        data = txt_importer(path_file)
+        output = {
+            "nome_do_arquivo": path_file,
+            "qtd_linhas": len(data),
+            "linhas_do_arquivo": data,
+        }
+        return sys.stdout.write(str(output))
 
 
 def remove(instance):
