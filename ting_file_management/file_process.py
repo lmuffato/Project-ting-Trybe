264f21e936b7c6cde7cc4c93908789c0d0766a00
Marcelo Maurício <mmauricio.design@gmail.com>
2022-02-04 16:48:17 -0300

    Feat 3: implemented a method to process a instance in a queue

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,8 +1,20 @@
 from ting_file_management.file_management import txt_importer
+import sys
 
 
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+
+    txt_file = txt_importer(path_file)
+
+    file_processed = {
+      "nome_do_arquivo": path_file,
+      "qtd_linhas": len(txt_file),
+      "linhas_do_arquivo": txt_file,
+    }
+
+    instance.enqueue(file_processed)
+
+    return sys.stdout.write(str(file_processed))
 
 
 def remove(instance):
