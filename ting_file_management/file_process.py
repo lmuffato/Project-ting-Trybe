843c290d26950d8414d83c57ef2d898f4d251cb2
Gabriel <g.pereira.a.m@gmail.com>
2022-02-04 17:00:16 -0300

    feat(Req 3) - :sparkle: Requisito 3 feito

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,36 @@
+import sys
+# from queue import Queue
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    name_file = path_file.strip("../")
+    length = len(instance)
+    importer = txt_importer(path_file)
+
+    if length == 0:
+        file = {
+            "nome_do_arquivo": path_file.strip("../"),
+            "qtd_linhas": len(importer),
+            "linhas_do_arquivo": importer
+        }
+
+        instance.enqueue(file)
+        sys.stdout.write(str(file) + '\n')
+
+    if length:
+        search = instance.search(length)
+        name_file_queue = search["nome_do_arquivo"]
+
+        if name_file != name_file_queue:
+            file = {
+                "nome_do_arquivo": path_file.strip("../"),
+                "qtd_linhas": len(importer),
+                "linhas_do_arquivo": importer
+            }
+
+            instance.enqueue(file)
+            sys.stdout.write(str(file) + '\n')
 
 
 def remove(instance):
@@ -8,3 +39,13 @@ def remove(instance):
 
 def file_metadata(instance, position):
     """Aqui irá sua implementação"""
+
+# fila = Queue()
+
+# pathOne = "../test.txt"
+# pathTwo = "../hello.txt"
+
+# process(pathOne, fila)
+# process(pathOne, fila)
+# print(process(pathTwo, fila))
+# print(len(fila))
