0b9b36ef013bec3769706e9382dd1ca77e6e975f
Rafael Geronimo <git_config_user_email>
2022-02-04 15:57:18 -0300

    feat: req6.1 | `exists_word` function | validate `exists_word` function finds word successfully

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,28 @@
+# from ting_file_management.file_process import process
+# from ting_file_management.queue import Queue
+
+
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    for index in range(instance.__len__()):
+        file_data = instance.search(index)
+        data = {
+            'palavra': word,
+            'arquivo': file_data['nome_do_arquivo'],
+            'ocorrencias': []
+        }
+        index = 0
+        for line in file_data['linhas_do_arquivo']:
+            index += 1
+            if word in line:
+                data['ocorrencias'].append({'linha': index})
+        return [data]
 
 
 def search_by_word(word, instance):
     """Aqui irá sua implementação"""
+
+
+# project = Queue()
+# process('statics/nome_pedro.txt', project)
+# word = exists_word('Pedro', project)
+# print(word)
