8581caa4e178a8461da0975b4b3b5d21df4060cc
Marcelo Maurício <mmauricio.design@gmail.com>
2022-02-04 15:34:16 -0300

    Feat 2: create a method to return a txt file into a list line by line

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,13 @@
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    content_file = []
+
+    with open(path_file, mode="r") as file:
+        text_content = [line.rstrip('\n') for line in file]
+
+        content_file = text_content
+        file.close()
+
+    return content_file
+
+# Conseguir extrair as linhas sem os espaços com a explicação:
+# https://stackoverflow.com/questions/3277503/how-to-read-a-file-line-by-line-into-a-list
