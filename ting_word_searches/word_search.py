8d5d2961c72ce0f21829487c0a664dc0697131d9
Hernanne Domiciano <78621976+hernannegp@users.noreply.github.com>
2022-02-04 16:08:23 -0300

    feat: req06

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,22 @@
+from ting_file_management.file_management import txt_importer
+
+
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    output = list()
+    count = 0
+    for path_files in instance.list:
+        data = txt_importer(path_files)
+        for phrase in data:
+            if word in phrase:
+                count += 1
+                linha = [{"linha": count}]
+                data_format = {
+                    "palavra": word,
+                    "arquivo": path_files,
+                    "ocorrencias": linha
+                }
+                output.append(data_format)
+    return output
 
 
 def search_by_word(word, instance):
