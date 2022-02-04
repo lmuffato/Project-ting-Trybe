edad62e4fb8e84db81f0ea46be5bbbc81a7e2ca1
MaxRudim <max.hermsdorff@gmail.com>
2022-02-04 20:33:34 -0300

    Requisito 03

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,23 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    # """Aqui irá sua implementação"""
+    # print(instance.search(0))
+    # print(path_file)
+    for index in range(len(instance)):
+        if instance.search(index)['nome_do_arquivo'] == path_file:
+            return None
+
+    text = txt_importer(path_file)
+    data = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(text),
+        "linhas_do_arquivo": text,
+    }
+    instance.enqueue(data)
+    return sys.stdout.write(f"{data}\n")
 
 
 def remove(instance):
