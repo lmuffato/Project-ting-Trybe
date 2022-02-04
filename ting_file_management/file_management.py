ac44ccf8ec94c233aff51fa4eab4184c6069e6ee
MaxRudim <max.hermsdorff@gmail.com>
2022-02-04 17:40:41 -0300

    Requisito 02

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,3 +1,19 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
-#
+    # """Aqui irá sua implementação"""
+    if path_file.endswith(".txt"):
+        try:
+            with open(path_file, mode="r") as file:
+                return file.read().splitlines()
+        except FileNotFoundError:
+            sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
+    else:
+        sys.stderr.write("Formato inválido\n")
+
+# Ref. para printar mensagens de erro stderr:
+# https://stackoverflow.com/questions/5574702/how-to-print-to-stderr-in-python
+
+# Ref. para quebrar linhas nos limites de cada string:
+# https://www.geeksforgeeks.org/python-string-splitlines-method/
