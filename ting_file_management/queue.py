ca64a6103d11c324b7b10e15ef67c0cc09b4bab4
MaxRudim <max.hermsdorff@gmail.com>
2022-02-03 21:44:47 -0300

    Requisito 01

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -1,15 +1,36 @@
 class Queue:
     def __init__(self):
-        """Inicialize sua estrutura aqui"""
+        # """Inicialize sua estrutura aqui"""
+        self._data = list()
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        # """Aqui irá sua implementação"""
+        return len(self._data)
+
+    def is_empty(self):         # indica se a pilha esta vazia ou não
+        return not bool(self.__len__)
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        # """Aqui irá sua implementação"""
+        self._data.append(value)
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        # """Aqui irá sua implementação"""
+        if self.is_empty():
+            return None
+
+        # O primeiro elemento é o que está a mais tempo na fila: FIFO
+        value = self._data[0]
+        del self._data[0]
+        return value
 
     def search(self, index):
-        """Aqui irá sua implementação"""
+        # """Aqui irá sua implementação"""
+        if self.is_empty():
+            return None
+
+        if index < 0:
+            raise IndexError
+
+        value = self._data[index]
+        return value
