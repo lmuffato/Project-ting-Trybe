dc6fcaa6a0db3bf5f8d6829371a7e3cad8f4d49c
Renan Oliveira <oliveira.renan.rg@gmail.com>
2022-02-04 12:11:03 -0300

    feat: start seconde requirement

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,14 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    try:
+        log = list()
+        if path_file.endswith('.txt'):
+            with open(path_file) as file:
+                log = file.read().split('\n')
+                return log
+        else:
+            print("Formato inválido", file=sys.stderr)
+    except FileNotFoundError:
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
