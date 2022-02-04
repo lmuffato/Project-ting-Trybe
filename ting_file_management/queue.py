e75420451000cdd45d3af16e339249cf38b0902c
Michael <michaelpetterson06.com.br>
2022-02-04 10:44:41 -0300

    req 01

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -1,15 +1,20 @@
 class Queue:
     def __init__(self):
-        """Inicialize sua estrutura aqui"""
+        self.data = []
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        return len(self.data)
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        self.data.append(value)
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        deleted = self.data[0]
+        self.data.pop(0)
+        return deleted
 
     def search(self, index):
-        """Aqui irá sua implementação"""
+        if 0 <= index < len(self.data):
+            return self.data[index]
+        else:
+            raise IndexError
