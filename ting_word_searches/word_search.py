001e0d9452b7613be78de1625657618ae9f148f9
MarcioFujita <hanker78@gmail.com>
2022-02-04 17:41:36 -0300

    feat: Requisito 6

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,24 @@
 def exists_word(word, instance):
     """Aqui irá sua implementação"""
+    file = instance.dequeue()
+    result = []
+    lower_case_word = word.lower()
+
+    for index, line in enumerate(file["linhas_do_arquivo"]):
+        lower_case_line = line.lower()
+        if lower_case_word in lower_case_line:
+            result.append({"linha": index + 1})
+
+    if not len(result):
+        return result
+
+    return [
+        {
+            "palavra": word,
+            "arquivo": file["nome_do_arquivo"],
+            "ocorrencias": result,
+        }
+    ]
 
 
 def search_by_word(word, instance):
