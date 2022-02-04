f21d25af4fdd045c1c98ae4f0bd7692316c1ad3a
giovanni-maldonado <giovannitadeu08@gmail.com>
2022-02-04 16:45:37 -0300

    feat: requisito 6

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,22 @@
+from ting_file_management.file_management import txt_importer
+
+
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    ocurrences = list()
+    count = 0
+    for path_file in instance.list:
+        data = txt_importer(path_file)
+        for line in data:
+            if word in line:
+                count += 1
+                linha = [{"linha": count}]
+                formated_data = {
+                    "palavra": word,
+                    "arquivo": path_file,
+                    "ocorrencias": linha
+                }
+                ocurrences.append(formated_data)
+    return ocurrences
 
 
 def search_by_word(word, instance):
