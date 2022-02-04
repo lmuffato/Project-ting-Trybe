2818e927ed2df47985680e8fdf6631e8c4cc207a
Viegas <mvrdgs@gmail.com>
2022-02-04 07:23:59 -0300

    requisito 7

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -11,7 +11,7 @@ def exists_word(word, instance):
         }
 
         for index, line in enumerate(file["linhas_do_arquivo"]):
-            if word in line:
+            if word.lower() in line.lower():
                 data["ocorrencias"].append({"linha": index + 1})
 
         if len(data["ocorrencias"]):
@@ -21,4 +21,24 @@ def exists_word(word, instance):
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    result = []
+
+    for position in range(len(instance)):
+        file = instance.search(position)
+
+        data = {
+            "palavra": word,
+            "arquivo": file["nome_do_arquivo"],
+            "ocorrencias": [],
+        }
+
+        for index, line in enumerate(file["linhas_do_arquivo"]):
+            if word.lower() in line.lower():
+                data["ocorrencias"].append(
+                    {"linha": index + 1, "conteudo": line}
+                )
+
+        if len(data["ocorrencias"]):
+            result.append(data)
+
+    return result
