0fa2cc9ae7cc6314b9f98ffacf568febb9c417b8
Felipe Muller <felipeavmuller@gmail.com>
2022-02-04 18:17:19 -0300

    1.1 enqueue function

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -1,12 +1,12 @@
 class Queue:
     def __init__(self):
-        """Inicialize sua estrutura aqui"""
+        self._data = list()
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        return len(self._data)
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        self._data.append(value)
 
     def dequeue(self):
         """Aqui irá sua implementação"""
