deb62ed7f2776e73ad362aa5c2f803ab67cb4c55
Felipe Flores <felipefloresvendas@gmail.com>
2022-02-03 21:04:17 -0300

    feat: requirement 2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,13 @@
+# https://www.w3schools.com/python/ref_string_splitlines.asp
+
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if not path_file.endswith('.txt'):
+        return sys.stderr.write('Formato inválido\n')
+    try:
+        with open(path_file, mode='r') as file:
+            return list(file.read().splitlines())
+    except FileNotFoundError:
+        sys.stderr.write(f'Arquivo {path_file} não encontrado\n')
