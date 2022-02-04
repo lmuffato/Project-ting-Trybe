9b6c733ed9b40e3ad3d4980c8b3cb565bb5c05c0
Hudson Carlos <hc88806@gmail.com>
2022-02-04 15:10:10 -0400

    req 2 done

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -2,15 +2,15 @@ import sys
 
 
 def txt_importer(path_file):
-    if path_file.endswith(".txt") == False:
+    if path_file.endswith(".txt") is False:
         return sys.stderr.write("Formato inválido\n")
 
     try:
-        arq = open(path_file, "r")
+        with open(path_file) as file:
+            arq = file.read()
     except FileNotFoundError:
         return sys.stderr.write(
             f"Arquivo {path_file} não encontrado\n"
         )
     else:
-        arq = arq.read()
         return arq.split("\n")
