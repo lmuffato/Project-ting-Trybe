60741c137cef18fded534f3ab548e37d4cdddda0
Hudson Carlos <hc88806@gmail.com>
2022-02-04 18:30:15 -0400

    req 6

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,20 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    line = 0
+    result = []
+    for element in instance.data:
+        search = {
+            "palavra": word,
+            "arquivo": element['nome_do_arquivo'],
+            "ocorrencias": []
+        }
+        for phrase in element["linhas_do_arquivo"]:
+            if word in phrase:
+                line += 1
+                search["ocorrencias"].append({"linha": line})
+        if len(search["ocorrencias"]) > 0:
+            result.append(search)
+
+    return result
 
 
 def search_by_word(word, instance):
