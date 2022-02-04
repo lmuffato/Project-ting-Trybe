193d2e5c6a665dbbc7952f93d0f189a0848534fb
Felipe Muller <felipeavmuller@gmail.com>
2022-02-04 19:26:53 -0300

    2.2 erro caso o arquivo nao seja encontrado

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -5,6 +5,9 @@ def txt_importer(path_file):
     if not path_file.endswith('txt'):
         print('Formato inválido', file=sys.stderr)
 
-    with open(path_file, 'r') as file:
-        content = file.read()
-        return content.split('\n')
+    try:
+        with open(path_file, 'r') as file:
+            content = file.read()
+            return content.split('\n')
+    except FileNotFoundError:
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
