037f051e908722327fa8d1f9f36cda15c1b27204
Carlos Sá <carlos25xrm@gmail.com>
2022-02-04 18:36:37 -0300

    feat: remove o arquivo processado informando mensagens de confirmação ou erro

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -14,7 +14,7 @@ def process(path_file, instance):
     # Validações e leitura do TXT baseado na função "txt_importer"
     importar_txt = txt_importer(path_file)
 
-    # Formata as informaçõe de acordo com o Readme
+    # Formata as informações de acordo com o Readme
     txt_processado = {
         'nome_do_arquivo': path_file,
         'qtd_linhas': len(importar_txt),
@@ -23,12 +23,24 @@ def process(path_file, instance):
 
     # Coloca a nova instância (Arquivo processado) na fila
     instance.enqueue(txt_processado)
+
     # Reademe pede que cada saída seja enviada pelo stdout
+    # Referência de uso do stdout:
+    # https://www.geeksforgeeks.org/sys-stdout-write-in-python
     sys.stdout.write(str(txt_processado))
 
 
 def remove(instance):
-    """Aqui irá sua implementação"""
+    # Lembrete: IF precisa de um return para parar a execução do código!
+    if not instance:
+        return sys.stdout.write("Não há elementos\n")
+        # O "\n" acima está somente no teste, não no readme!
+
+    # Tirar da fila o arquivo especificado pela chave "nome_do_arquivo"
+    tirar_da_fila = instance.dequeue()["nome_do_arquivo"]
+    sys.stdout.write(f"Arquivo {tirar_da_fila} removido com sucesso\n")
+    # Acima não é necessário return porque não há nada mais
+    # a ser executado, acabou a função de remover da fila!
 
 
 def file_metadata(instance, position):
