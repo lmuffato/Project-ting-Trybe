28160a84733b934d1fff66785695298561d5cda4
Hudson Carlos <hc88806@gmail.com>
2022-02-04 14:53:23 -0400

    req 2 done

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,16 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if path_file.endswith(".txt") == False:
+        return sys.stderr.write("Formato inválido\n")
+
+    try:
+        arq = open(path_file, "r")
+    except FileNotFoundError:
+        return sys.stderr.write(
+            f"Arquivo {path_file} não encontrado\n"
+        )
+    else:
+        arq = arq.read()
+        return arq.split("\n")
