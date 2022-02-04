758626d313f5a95fef7290c92b9d02618e65f3d1
Marcelo Maurício <mmauricio.design@gmail.com>
2022-02-04 16:09:57 -0300

    Feat 2: if the path_file not exist, launch a stderr message

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,13 +1,21 @@
+import sys
+
+
 def txt_importer(path_file):
     content_file = []
 
-    with open(path_file, mode="r") as file:
-        text_content = [line.rstrip('\n') for line in file]
+    try:
+        with open(path_file, mode="r") as file:
+            text_content = [line.rstrip('\n') for line in file]
 
-        content_file = text_content
-        file.close()
+            content_file = text_content
+            file.close()
 
-    return content_file
+    except FileNotFoundError:
+        sys.stderr.write(f'Arquivo {path_file} não encontrado\n')
+
+    finally:
+        return content_file
 
 # Conseguir extrair as linhas sem os espaços com a explicação:
 # https://stackoverflow.com/questions/3277503/how-to-read-a-file-line-by-line-into-a-list
