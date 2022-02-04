50ed528f612c8d676b57c45e9e4a936a55c51dc8
nascjoao <oi@joaonasc.dev>
2022-02-03 21:01:40 -0300

    feat: implement search_by_word in word_search

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -26,4 +26,25 @@ def exists_word(word, instance):
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    output = list()
+    word_not_found = True
+    for path_file in instance.list:
+        word_report = {
+            "palavra": word,
+            "arquivo": path_file,
+            "ocorrencias": list()
+        }
+        txt = txt_importer(path_file)
+        line = 1
+        for row in txt:
+            if word.lower() in row.lower():
+                word_not_found = False
+                word_report['ocorrencias'].append({
+                    "linha": line,
+                    "conteudo": row
+                })
+            line += 1
+        output.append(word_report)
+        if word_not_found:
+            return list()
+        return output
