fd505684f5436b6ae479cbe5b2881820e97492a8
Marcus Hüsemann <mchusemann@gmail.com>
2022-02-04 15:48:24 -0300

    feat: implementa funçao exist_word, que busca ocorrencias de uma palavra em todos os arquivos

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,33 @@
+import re
+
+
+def search_occurrences_in_file(word, file):
+    lines_occurrences = []
+    for key, line in enumerate(file['linhas_do_arquivo']):
+        text = re.search(word, line, re.IGNORECASE)
+        if text:
+            lines_occurrences.append({"linha": key + 1})
+    return lines_occurrences
+
+
+def get_occurrences(word, instance):
+    occurrences = []
+    for index in range(len(instance)):
+        file = instance.search(index)
+        lines_occurrences = search_occurrences_in_file(word, file)
+        if lines_occurrences:
+            occurrences.append({
+                "arquivo": file['nome_do_arquivo'],
+                "palavra": word,
+                "ocorrencias": lines_occurrences
+            })
+    return occurrences
+
+
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    occurrences = get_occurrences(word, instance)
+    return occurrences
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    pass
