9d199f5c727ea4fe89a032752cd2a91c49484866
Luiz <luizfvaccarif@gmail.com>
2022-02-05 13:36:32 -0300

    feat: req 01

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -1,15 +1,20 @@
 class Queue:
     def __init__(self):
-        
+        self._data = list()
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        return len(self._data)
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        self._data.append(value)
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        value = self._data[0]
+        del self._data[0]
+        return value
 
     def search(self, index):
-        """Aqui irá sua implementação"""
+        if index not in range(len(self._data)):
+            raise IndexError
+        value = self._data[index]
+        return value
