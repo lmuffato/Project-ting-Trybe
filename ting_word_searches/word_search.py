b84e3385ff97fac12a6234b23cd311a59ba99c43
Michael <michaelpetterson06.com.br>
2022-02-05 18:32:59 -0300

    req 06-07

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,32 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    searched = [{"palavra": word, "arquivo": "", "ocorrencias": []}]
+    occurrences = []
+    data = instance.data
+
+    for item in data:
+        searched[0]["arquivo"] = item["nome_do_arquivo"]
+        lines = item["linhas_do_arquivo"]
+        for index, line in enumerate(lines):
+            if word.upper() in line.upper():
+                occurrences.append({"linha": index + 1})
+        searched[0]["ocorrencias"] = occurrences
+        if len(occurrences) > 0:
+            return searched
+    return []
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    searched = [{"palavra": word, "arquivo": "", "ocorrencias": []}]
+    occurrences = []
+    data = instance.data
+
+    for item in data:
+        searched[0]["arquivo"] = item["nome_do_arquivo"]
+        lines = item["linhas_do_arquivo"]
+        for index, line in enumerate(lines):
+            if word.upper() in line.upper():
+                occurrences.append({"linha": index + 1, "conteudo": line})
+        searched[0]["ocorrencias"] = occurrences
+        if len(occurrences) > 0:
+            return searched
+    return []
