70a24e8beac9d08f0661cc22ec23c74c9ad2272e
joaoAndradeJr <fullservicemanaus@gmail.com>
2022-02-04 21:03:41 -0400

    add: invalid format validation

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,15 @@
+import sys
+
+
+def validations(path_file):
+    extension = path_file.split(".")
+    if extension[-1] != "txt":
+        return sys.stderr.write("Formato inválido\n")
+
+    return True
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    validated = validations(path_file)
+    if validated is str:
+        return validated
