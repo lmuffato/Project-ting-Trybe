1f256cca631093656854c69e8e69c44b3bfb008f
GuilhermeMD <guilherme.md@gmail.com>
2022-02-05 20:44:58 -0300

    req 3.1

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,6 +1,18 @@
-def process(path_file, instance):
-    """Aqui irá sua implementação"""
+import sys
+from ting_file_management.file_management import txt_importer
 
+def process(path_file, instance):
+    for index in range(len(instance)):
+        if instance.search(index)["nome_do_arquivo"] == path_file:
+            return None
+    file = txt_importer(path_file)
+    data = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(file),
+        "linhas_do_arquivo": file,
+    }
+    instance.enqueue(data)
+    sys.stdout.write(str(data))
 
 def remove(instance):
     """Aqui irá sua implementação"""
