b09f814be12a365aeb46d3ee86f92c3763524efd
joaoAndradeJr <fullservicemanaus@gmail.com>
2022-02-04 21:13:49 -0400

    add: file not found error

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,15 +1,22 @@
 import sys
 
 
+def check_if_path_exists(path):
+    try:
+        open(path, "r")
+    except FileNotFoundError:
+        sys.stderr.write(f"Arquivo {path} não encontrado\n")
+
+
 def validations(path_file):
     extension = path_file.split(".")
     if extension[-1] != "txt":
         return sys.stderr.write("Formato inválido\n")
 
-    return True
+    check_if_path_exists(path_file)
+    return False
 
 
 def txt_importer(path_file):
-    validated = validations(path_file)
-    if validated is str:
-        return validated
+    if not validations(path_file):
+        print("entrou")
