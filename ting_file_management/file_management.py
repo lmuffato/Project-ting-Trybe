2e8f54c33a55b253accc802207468039566b735f
GuilhermeMD <guilherme.md@gmail.com>
2022-02-05 20:26:02 -0300

    req 02

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,15 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação ulllltimoooooooo"""
+    text = []
+    if not path_file.endswith(".txt"):
+        sys.stderr.write("Formato inválido\n")
+    try:
+        with open(path_file) as file:
+            for line in file:
+                text.append(line.replace("\n", ""))
+            return text
+    except FileNotFoundError:
+        #https://www.delftstack.com/pt/howto/python/python-print-to-stderr/
+        sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
