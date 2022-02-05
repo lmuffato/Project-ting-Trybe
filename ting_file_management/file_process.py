f0831e9a833afc57a34e9852deb8d07f7169bbd3
Renzo Sevilha <sevilharenzo@gmail.com>
2022-02-05 15:45:19 -0300

    feat: req3

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,26 @@
+from ting_file_management import file_management
+import sys
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    pathes = [
+        instance.search(index)['nome_do_arquivo']
+        for index in range(instance.__len__())
+    ]
+
+    if path_file in pathes:
+        return None
+
+    txt = file_management.txt_importer(path_file)
+
+    data = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(txt),
+        "linhas_do_arquivo": txt
+    }
+
+    sys.stdout.write(str(data))
+    instance.enqueue(data)
 
 
 def remove(instance):
