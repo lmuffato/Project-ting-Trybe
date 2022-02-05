549e7a15466629ec124af773f40b5f29f505240d
Lucas Lara <lucasml@live.com>
2022-02-05 17:44:05 -0300

    Refatorando importaçao requisito 2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,15 +1,15 @@
-from sys import stderr
+import sys
 
 
 def txt_importer(path_file):
     answer = []
     # https://www.w3schools.com/python/ref_string_endswith.asp
     if not path_file.endswith(".txt"):
-        stderr.write("Formato inválido\n")
+        sys.stderr.write("Formato inválido\n")
     try:
         with open(path_file) as file:
             for line in file:
                 answer.append(line.replace("\n", ""))
             return answer
     except FileNotFoundError:
-        stderr.write(f"Arquivo {path_file} não encontrado\n")
+        sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
