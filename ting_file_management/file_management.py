794e3bef427fc3ecdb3172bdcb5f18ed4b217f6e
Gustavo Sousa <gustavohl98@gmail.com>
2022-02-05 13:50:53 -0300

    Req 2 - implements function txt_importer

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,14 @@
+import sys
+
+
+# Print para strerr consultado em:
+# https://www.delftstack.com/pt/howto/python/python-print-to-stderr/
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if not path_file.endswith(".txt"):
+        print("Formato inválido", file=sys.stderr)
+    try:
+        with open(path_file, mode="r") as my_file:
+            content = my_file.read()
+            return content.split("\n")
+    except FileNotFoundError:
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
