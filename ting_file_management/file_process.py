1a83169a43b9c0fc913707f744cabbe030492fdd
Felipe Muller <felipeavmuller@gmail.com>
2022-02-04 21:59:51 -0300

    funcao process sem validacao 3.1

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,17 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    file_content = txt_importer(path_file)
+    lines_qty = len(file_content)
+    result = {
+        'nome_do_arquivo': path_file,
+        'qtd_linhas': lines_qty,
+        'linhas_do_arquivo': file_content
+    }
+    instance.enqueue(result)
+    print(result, file=sys.stdout)
 
 
 def remove(instance):
