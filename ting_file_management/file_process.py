80124f0d13b2b4db8e7b3c70bf48125fec642978
Gustavo Sousa <gustavohl98@gmail.com>
2022-02-05 14:36:48 -0300

    Req 3 - implements func process to process txt file

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,20 @@
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    text_content = txt_importer(path_file)
+
+    output = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(text_content),
+        "linhas_do_arquivo": text_content
+    }
+
+    if len(instance) > 0 and instance.search(0) == text_content:
+        return print(output)
+
+    instance.enqueue(text_content)
+    print(output)
 
 
 def remove(instance):
