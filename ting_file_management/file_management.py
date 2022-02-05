6cb4299be1aa50ae51bfb3f14657c9308553c5b7
Lucas Lara <lucasml@live.com>
2022-02-05 16:41:18 -0300

    Requisito 2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,15 @@
+from sys import stderr
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação."""
+    answer = []
+    # https://www.w3schools.com/python/ref_string_endswith.asp
+    if not path_file.endswith(".txt"):
+        stderr.write("Formato inválido\n")
+    try:
+        with open(path_file) as file:
+            for line in file:
+                answer.append(line.replace("\n", ""))
+            return answer
+    except FileNotFoundError:
+        stderr.write(f"Arquivo {path_file} não encontrado\n")
