3dab43041484e90c32be95d0cf7679e28da8c9e9
Felipe Muller <felipeavmuller@gmail.com>
2022-02-04 22:25:16 -0300

    6 - search

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -29,4 +29,8 @@ def remove(instance):
 
 
 def file_metadata(instance, position):
-    """Aqui irá sua implementação"""
+    try:
+        result = instance.search(position)
+        print(result)
+    except IndexError:
+        print('Posição inválida', file=sys.stderr)
