c673291c50ad0d6bacaf3e980f460e805b1458d8
Lucas Lara <lucasml@live.com>
2022-02-05 16:53:31 -0300

    Requisito 4

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -13,7 +13,11 @@ def process(path_file, instance):
 
 
 def remove(instance):
-    """Aqui irá sua implementação"""
+    if len(instance) != 0:
+        removed = instance.dequeue()
+        sys.stdout.write(f"Arquivo {removed} removido com sucesso\n")
+    else:
+        sys.stdout.write("Não há elementos\n")
 
 
 def file_metadata(instance, position):
