aef0c9a34ff1d9f95970f2e891265b7945a7b565
Lucas Lara <lucasml@live.com>
2022-02-05 16:47:25 -0300

    Requisito 3

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,15 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    instance.enqueue(path_file)
+    return print(
+        f"'nome_do_arquivo': '{path_file}'\n",
+        f"'qtd_linhas': {len(txt_importer(path_file))}\n",
+        f"'linhas_do_arquivo': {txt_importer(path_file)}\n",
+        file=sys.stdout,
+    )
 
 
 def remove(instance):
