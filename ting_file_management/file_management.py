9aeef1c968ab965fa8cec96b1b99c17b4e0b07cf
joaoAndradeJr <fullservicemanaus@gmail.com>
2022-02-04 21:24:42 -0400

    refactor: validate path on txt_importer function with tye/except

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,22 +1,18 @@
 import sys
 
 
-def check_if_path_exists(path):
-    try:
-        open(path, "r")
-    except FileNotFoundError:
-        sys.stderr.write(f"Arquivo {path} não encontrado\n")
-
-
 def validations(path_file):
     extension = path_file.split(".")
     if extension[-1] != "txt":
         return sys.stderr.write("Formato inválido\n")
 
-    check_if_path_exists(path_file)
     return False
 
 
 def txt_importer(path_file):
     if not validations(path_file):
-        print("entrou")
+        try:
+            data = open(path_file, "r")
+            print(data)
+        except FileNotFoundError:
+            sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
