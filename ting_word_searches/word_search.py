42e52121460fa22da69af6e07c16e0540f9976ff
Felipe Muller <felipeavmuller@gmail.com>
2022-02-04 22:56:40 -0300

    6 - exists word function

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,23 @@
+#  https://realpython.com/python-enumerate/
+
+
 def exists_word(word, instance):
-    """Aqui irá sua implementação."""
+    result = list()
+    for index in range(instance.__len__()):
+        current = instance.search(index)
+        data = {
+            'palavra': word,
+            'arquivo': current['nome_do_arquivo'],
+            'ocorrencias': []
+        }
+        for count, line in enumerate(current['linhas_do_arquivo']):
+            if word.upper() in line.upper():
+                data['ocorrencias'].append({
+                    'linha': count + 1
+                })
+        if len(data['ocorrencias']) > 0:
+            result.append(data)
+    return result
 
 
 def search_by_word(word, instance):
