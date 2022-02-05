3bc6034ce7e36ad92791927cc2f2282bb45c93f2
rcalvs <rafaelcalvette@gmail.com>
2022-02-05 13:58:39 -0300

    req 02 Ok

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,13 @@
+import sys
+
+
 def txt_importer(path_file):
     """Aqui irá sua implementação"""
+    if not path_file.endswith(".txt"):
+        return sys.stderr.write('Formato inválido\n')
+    try:
+        file = open(path_file, "r")
+        text = file.read().splitlines()
+        return text
+    except FileNotFoundError:
+        return sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
