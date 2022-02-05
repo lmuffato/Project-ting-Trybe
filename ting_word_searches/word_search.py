6e387f1b647172d7615be6a87a458cbb526dd40f
Felipe Muller <felipeavmuller@gmail.com>
2022-02-04 22:58:12 -0300

    7 - search by word - adiciona conteudo da linha na busca por palavra

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -21,4 +21,20 @@ def exists_word(word, instance):
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    result = list()
+    for index in range(instance.__len__()):
+        current = instance.search(index)
+        data = {
+            'palavra': word,
+            'arquivo': current['nome_do_arquivo'],
+            'ocorrencias': []
+        }
+        for count, line in enumerate(current['linhas_do_arquivo']):
+            if word.upper() in line.upper():
+                data['ocorrencias'].append({
+                    'linha': count + 1,
+                    'conteudo': line
+                })
+        if len(data['ocorrencias']) > 0:
+            result.append(data)
+    return result
