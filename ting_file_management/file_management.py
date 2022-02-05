18194c4415426249c360a5ec671fe185419006dc
Luiz <luizfvaccarif@gmail.com>
2022-02-05 19:50:25 -0300

    feat: req 02

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,15 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    try:
+        formatedText = list()
+        if '.txt' not in path_file:
+            return sys.stderr.write("Formato inválido\n")
+        with open(path_file) as f:
+            text = f.readlines()
+            for line in text:
+                formatedText.append(line.rstrip())
+        return formatedText
+    except FileNotFoundError:
+        return sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
