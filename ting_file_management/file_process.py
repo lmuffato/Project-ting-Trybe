9634fb7850c20e90a331017e80a407ba50723402
AndreBarroso <andre_barroso17@hotmail.com>
2022-02-05 14:09:20 -0300

    feat: requisito3

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,20 @@
+import sys
+
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    for i in range(len(instance)):
+        if instance.search(i)["nome_do_arquivo"] == path_file:
+            return None
+    file = txt_importer(path_file)
+    data = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(file),
+        "linhas_do_arquivo": file,
+    }
+    instance.enqueue(data)
+    sys.stdout.write(str(data))
 
 
 def remove(instance):
