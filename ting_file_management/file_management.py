162766d4936bd2eb8cfbc2d723b5777179e06915
Bruno Mendes <mendessbruno@gmail.com>
2022-02-05 16:51:44 -0300

    resolvido desafio 2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,17 @@
+import sys
+
+# Impressao de stderr encontrado no link:
+# https://stackoverflow.com/questions/5574702/how-to-print-to-stderr-in-python
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if ".txt" not in path_file:
+        return sys.stderr.write("Formato inválido\n")
+    try:
+        data = []
+        with open(path_file, "r") as file:
+            for row in file:
+                data.append(row.replace("\n", ""))
+        return data
+    except FileNotFoundError:
+        return sys.stderr.write("Arquivo " + path_file + " não encontrado\n")
