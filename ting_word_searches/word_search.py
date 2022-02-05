7d038c33ce964986f27a98acaea42bf1fb206811
Lucas Lara <lucasml@live.com>
2022-02-05 17:08:01 -0300

    Requisito 6

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,23 @@
+from ting_file_management.file_management import txt_importer
+
+
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    data = instance.search(0)
+    text_file = txt_importer(instance.search(0))
+    occur = []
+    answer = [
+        {
+            "palavra": f"{word}",
+            "arquivo": f"{data}",
+            "ocorrencias": occur,
+        }
+    ]
+    for index, row in enumerate(text_file):
+        if word in row:
+            occur.append({"linha": int(f"{index  + 1}")})
+        if occur:
+            return answer
+    return []
 
 
 def search_by_word(word, instance):
