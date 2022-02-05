8c10562dd4bd9fbfb3d6e5e0dbeeb3da250b5d32
Renzo Sevilha <sevilharenzo@gmail.com>
2022-02-05 15:08:21 -0300

    feat: req2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,13 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if not path_file.endswith(".txt"):
+        return sys.stderr.write("Formato inválido\n")
+
+    try:
+        with open(path_file) as txt:
+            txt_content = txt.read().splitlines()
+            return txt_content
+    except FileNotFoundError:
+        sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
