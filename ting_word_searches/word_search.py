9239c2e275576218aad3667fe6bfe7407b9de89b
Alexandre Moraes <alexandre.moraes@vilaapps.com>
2022-02-06 12:13:34 -0300

    feat req 6

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,21 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    data_list = list()
+    for item in instance.data:
+        data = {
+            "palavra": word,
+            "arquivo": item["nome_do_arquivo"],
+            "ocorrencias": []
+        }
+        data_list.append(data)
+        for index, line in enumerate(item["linhas_do_arquivo"]):
+            current_line = index + 1
+            if word.lower() in line.lower():
+                data_list[index]["ocorrencias"].append({
+                    "linha": current_line
+                })
+                if len(data_list[index]["ocorrencias"]) > 0:
+                    return data_list
+        return list()
 
 
 def search_by_word(word, instance):
