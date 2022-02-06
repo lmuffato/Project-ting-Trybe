8acb768eadf09b27c540855772a2689fbe91d69b
igson <igson660@gmail.com>
2022-02-06 17:49:50 -0500

    fix quesito 02

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -3,8 +3,8 @@ import sys
 
 def txt_importer(path_file):
     try:
-      if not path_file.endswith(".txt"):
-          return sys.stderr.write('Formato inválido\n')
-      return open(path_file, "r").read().splitlines()
+        if not path_file.endswith(".txt"):
+            return sys.stderr.write('Formato inválido\n')
+        return open(path_file, "r").read().splitlines()
     except FileNotFoundError:
         return sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
