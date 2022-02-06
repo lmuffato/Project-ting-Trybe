6368a1c49451199c3249ce4f63d8c71d2073bf5f
igson <igson660@gmail.com>
2022-02-06 17:48:18 -0500

    quesito 02

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,10 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    try:
+      if not path_file.endswith(".txt"):
+          return sys.stderr.write('Formato inválido\n')
+      return open(path_file, "r").read().splitlines()
+    except FileNotFoundError:
+        return sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
