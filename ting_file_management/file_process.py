5b2b3574d67a09f5c124e4ca07904494b3d35378
Luiz <luizfvaccarif@gmail.com>
2022-02-05 22:37:00 -0300

    feat: req 04

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -17,7 +17,11 @@ def process(path_file, instance):
 
 
 def remove(instance):
-    """Aqui irá sua implementação"""
+    if len(instance._data) == 0:
+        return print("Não há elementos")
+    path_file = instance._data[0]['nome_do_arquivo']
+    Queue.dequeue(instance)
+    return print(f'Arquivo {path_file} removido com sucesso')
 
 
 def file_metadata(instance, position):
