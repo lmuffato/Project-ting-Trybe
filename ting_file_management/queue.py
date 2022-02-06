5e7ad1d1a9c2f01fdaae488981eadab48a5be0ef
luanlsr <luanfswd@gmail.com>
2022-02-06 11:17:36 -0300

    req 2 finalizado

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -1,15 +1,19 @@
 class Queue:
     def __init__(self):
-        """Inicialize sua estrutura aqu i"""
+        self.items = []
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        return len(self.items)
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        self.items.append(value)
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        return self.items.pop(0)
 
     def search(self, index):
-        """Aqui irá sua implementação"""
+        if index < 0:
+            raise IndexError
+        return self.items[index]
+
+# requisito feito com https://www.geeksforgeeks.org/stack-in-python/
