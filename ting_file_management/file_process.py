1885ab7287338efbd35cd39b12b8fd15b08ab9a7
luanlsr <luanfswd@gmail.com>
2022-02-06 11:42:57 -0300

    req 4 finalizado

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -18,7 +18,14 @@ def process(path_file, instance):
 
 
 def remove(instance):
-    """Aqui irá sua implementação"""
+    if instance.__len__() == 0:
+        return sys.stdout.write("Não há elementos\n")
+    try:
+        dados = instance.dequeue()
+        path_file = dados["nome_do_arquivo"]
+        print(f"Arquivo {path_file} removido com sucesso", file=sys.stdout)
+    except NotImplementedError:
+        raise Exception
 
 
 def file_metadata(instance, position):
