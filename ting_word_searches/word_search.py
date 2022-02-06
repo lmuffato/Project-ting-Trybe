2db1d41a3d13ba565b58197c72c8f534e5892f96
Luciano Amâncio <madruga665@hotmail.com>
2022-02-06 20:17:33 -0300

    requisito 6.2

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,17 @@
+from ting_file_management.queue import Queue
+
+
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    result = {"palavra": word, "arquivo": "nome_do_arquivo", "ocorrencias": []}
+    if len(result["ocorrencias"]) > 0:
+        return result
+    return list()
 
 
 def search_by_word(word, instance):
     """Aqui irá sua implementação"""
+
+
+Queue = Queue()
+
+print(exists_word("Acima", Queue))
