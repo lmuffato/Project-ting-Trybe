d76c0a2ab7fa31e4d6af26d07172ab5defe1ba23
GuilhermeMD <guilherme.md@gmail.com>
2022-02-06 16:22:37 -0300

    fix exists_word req 06

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,17 +1,17 @@
-def exists_word(word, instance):
+def exists_word(word: str, instance):
     words_qty = list()
-    for file in instance.items:
+    for single_file in instance.data:
         data = {
             "palavra": word,
-            "arquivo": file["nome_do_arquivo"],
+            "arquivo": single_file["nome_do_arquivo"],
             "ocorrencias": []
         }
         words_qty.append(data)
-        for index, line in enumerate(file["linhas_do_arquivo"]):
-            current_line = index + 1
-            if word.lower() in line.lower():
+        for index, line in enumerate(single_file["linhas_do_arquivo"]):
+            line_index = index + 1
+            if word.casefold() in line.casefold():
                 words_qty[index]["ocorrencias"].append({
-                    "linha": current_line
+                    "linha": line_index
                 })
                 if len(words_qty[index]["ocorrencias"]) > 0:
                     return words_qty
