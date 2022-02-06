d841bb2bbe840f7c50bb5d3aed509e9c87c066d2
Lucas André <lucas.cancado89@gmail.com>
2022-02-06 17:02:00 -0300

    req03

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,20 @@
-def process(path_file, instance):
-    """Aqui irá sua implementação"""
+from ting_file_management.file_management import txt_importer
+from ting_file_management.queue import Queue
+
+
+queue_obj = Queue()
+
+
+def process(path_file, queue_obj):
+    for linha in queue_obj.queue:
+        if linha['nome_do_arquivo'] == path_file:
+            return
+    dictio = {}
+    dictio['nome_do_arquivo'] = path_file
+    dictio['qtd_linhas'] = len(txt_importer(path_file))
+    dictio['linhas_do_arquivo'] = txt_importer(path_file)
+    queue_obj.enqueue(dictio)
+    print(dictio)
 
 
 def remove(instance):
