ad14345551082bd1329a5e71b913b5f47935b2ad
Luciano Amâncio <madruga665@hotmail.com>
2022-02-06 14:15:26 -0300

    requisito 4

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -6,12 +6,12 @@ from ting_file_management.queue import Queue
 def process(path_file, instance):
     data = txt_importer(path_file)
     result = {
-        'nome_do_arquivo': path_file,
-        'qtd_linhas': len(data),
-        'linhas_do_arquivo': data,
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(data),
+        "linhas_do_arquivo": data,
     }
     for index in instance.queue:
-        if result['nome_do_arquivo'] == index['nome_do_arquivo']:
+        if result["nome_do_arquivo"] == index["nome_do_arquivo"]:
             return None
     else:
         instance.enqueue(result)
@@ -19,11 +19,11 @@ def process(path_file, instance):
 
 
 def remove(instance):
-    if len(instance.queue) == 0:
-        sys.stderr.write("Não há elementos")
-
-    remove_item = instance.dequeue()['nome_do_arquivo']
-    sys.stdout.write(f"Arquivo {remove_item} removido com sucesso\n")
+    if instance.__len__() > 0:
+        remove_item = instance.dequeue()["nome_do_arquivo"]
+        sys.stdout.write(f"Arquivo {remove_item} removido com sucesso\n")
+    else:
+        sys.stdout.write("Não há elementos\n")
 
 
 def file_metadata(instance, position):
