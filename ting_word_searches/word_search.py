97d7d59499cd1236e0d1bc2b4eaaf70459ead585
Lucas André <lucas.cancado89@gmail.com>
2022-02-06 17:59:40 -0300

    req06: done

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,18 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    result = []
+    for i in range(instance.__len__()):
+        item = instance.search(i)
+        dictio = {
+            'palavra': word,
+            'arquivo': item['nome_do_arquivo'],
+            'ocorrencias': []
+        }
+        for index in range(item['qtd_linhas']):
+            if word in item['linhas_do_arquivo'][index]:
+                dictio['ocorrencias'].append({'linha': index + 1})
+        if len(dictio['ocorrencias']) > 0:
+            result.append(dictio)
+    return result
 
 
 def search_by_word(word, instance):
