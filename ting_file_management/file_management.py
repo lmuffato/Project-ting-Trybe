3727ec5d315ab0c39ed579576bcb37bb79d94548
luanlsr <luanfswd@gmail.com>
2022-02-06 10:44:17 -0300

    req 1 finalizado

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,12 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    # verificar se o arquivo termina com .txt
+    if not path_file.endswith(".txt"):
+        return sys.stderr.write('Formato inválido\n')
+    try:
+        file = open(path_file, "r")
+        return file.read().split('\n')
+    except FileNotFoundError:
+        return sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
