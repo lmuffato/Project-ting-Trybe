5dbd95f2b229ae26ed765e17aeb36f672861d968
Luiz <luizfvaccarif@gmail.com>
2022-02-05 22:03:26 -0300

    feat: req 03

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,19 @@
+from .queue import Queue
+from .file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    file_content = txt_importer(path_file)
+    json = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(file_content),
+        "linhas_do_arquivo": file_content
+    }
+    for i in instance._data:
+        if i['nome_do_arquivo'] == path_file:
+            return json
+    Queue.enqueue(instance, json)
+    return print(json)
 
 
 def remove(instance):
