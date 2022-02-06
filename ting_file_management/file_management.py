2831524f7b1551651c6398fba65984931d59c72e
Alexandre Moraes <alexandre.moraes@vilaapps.com>
2022-02-06 11:10:33 -0300

    feat req 2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,12 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá  implementação"""
+    if not path_file.endswith(".txt"):
+        return sys.stderr.write('Formato inválido\n')
+    try:
+        file = open(path_file, "r")
+        news = file.read().splitlines()
+        return news
+    except FileNotFoundError:
+        return sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
