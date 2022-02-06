b58cd5188f90c1323b7398ec834dcf43653570f2
luanlsr <luanfswd@gmail.com>
2022-02-06 11:29:07 -0300

    req 3 finalizado

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,20 @@
+import sys
+
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    file = txt_importer(path_file)
+    for index in range(len(instance)):
+        if instance.search(index)["nome_do_arquivo"] == path_file:
+            return None
+    dados = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(file),
+        "linhas_do_arquivo": file,
+    }
+    instance.enqueue(dados)
+    sys.stdout.write(str(dados))
 
 
 def remove(instance):
