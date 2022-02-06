573a4be27f417c004cc7ce295bc55bb565729995
Alexandre Moraes <alexandre.moraes@vilaapps.com>
2022-02-06 11:25:08 -0300

    feat req 3

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,19 @@
+from ting_file_management.file_management import txt_importer
+import sys
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    for index in range(len(instance)):
+        if instance.search(index)["nome_do_arquivo"] == path_file:
+            return None
+    data = txt_importer(path_file)
+    output = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(data),
+        "linhas_do_arquivo": data,
+    }
+    instance.enqueue(output)
+    sys.stdout.write(str(output))
 
 
 def remove(instance):
