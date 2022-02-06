bc9663313da58a455a9d46eb30e49b2b97ee5f36
Gustavo Sousa <gustavohl98@gmail.com>
2022-02-06 16:01:05 -0300

    Req 6 done - implements function existis word

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,26 @@
 def exists_word(word, instance):
-    return []
+    outputs = []
+    total_files = len(instance)
+
+    for index in range(total_files):
+        curr_file = instance.search(index)
+        lines = curr_file["linhas_do_arquivo"]
+
+        output = {
+            "palavra": word,
+            "arquivo": curr_file["nome_do_arquivo"],
+            "ocorrencias": []
+        }
+
+        for lines_index in range(len(lines)):
+            if word.lower() in lines[lines_index].lower():
+                output["ocorrencias"].append({
+                    "linha": lines_index + 1,
+                })
+        if len(output["ocorrencias"]) > 0:
+            outputs.append(output)
+
+    return outputs
 
 
 def search_by_word(word, instance):
