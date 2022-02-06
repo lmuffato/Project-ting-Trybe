409f5827bb0183de341d4513f934cb85f60cf92a
Gustavo Sousa <gustavohl98@gmail.com>
2022-02-06 16:02:54 -0300

    Req 7 done - implements function search by word

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -24,4 +24,26 @@ def exists_word(word, instance):
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    outputs = []
+    total_files = len(instance)
+
+    for index in range(total_files):
+        curr_file = instance.search(index)
+        lines = curr_file["linhas_do_arquivo"]
+
+        output = {
+            "palavra": word,
+            "arquivo": curr_file["nome_do_arquivo"],
+            "ocorrencias": []
+        }
+
+        for lines_index in range(len(lines)):
+            if word.lower() in lines[lines_index].lower():
+                output["ocorrencias"].append({
+                    "linha": lines_index + 1,
+                    "conteudo": lines[lines_index]
+                })
+        if len(output["ocorrencias"]) > 0:
+            outputs.append(output)
+
+    return outputs
