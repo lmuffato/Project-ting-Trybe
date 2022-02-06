f8259cc2b2a945c74714038c5dd97b7042a96a4f
Luiz <luizfvaccarif@gmail.com>
2022-02-05 23:47:56 -0300

    feat: req 06

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,23 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    data_list = []
+    ocurrencies = []
+    print(instance._data)
+    for text in instance._data:
+        print(text)
+        line_number = 0
+        for line in text['linhas_do_arquivo']:
+            line_number += 1
+            if word in line:
+                lineAndContent = {"linha": line_number}
+                ocurrencies.append(lineAndContent)
+        if len(ocurrencies) == 0:
+            return ocurrencies
+        text_data = {
+            "palavra": word,
+            "arquivo": text['nome_do_arquivo'],
+            "ocorrencias": ocurrencies, }
+        data_list.append(text_data)
+    return data_list
 
 
 def search_by_word(word, instance):
