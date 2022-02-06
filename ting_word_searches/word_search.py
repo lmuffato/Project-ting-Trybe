c9e745d823ec7ade91c8a7623c838de9ff62e83f
GuilhermeMD <guilherme.md@gmail.com>
2022-02-06 16:03:37 -0300

    exist_word implementation req 06

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,21 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    words_qty = list()
+    for file in instance.items:
+        data = {
+            "palavra": word,
+            "arquivo": file["nome_do_arquivo"],
+            "ocorrencias": []
+        }
+        words_qty.append(data)
+        for index, line in enumerate(file["linhas_do_arquivo"]):
+            current_line = index + 1
+            if word.lower() in line.lower():
+                words_qty[index]["ocorrencias"].append({
+                    "linha": current_line
+                })
+                if len(words_qty[index]["ocorrencias"]) > 0:
+                    return words_qty
+        return list()
 
 
 def search_by_word(word, instance):
