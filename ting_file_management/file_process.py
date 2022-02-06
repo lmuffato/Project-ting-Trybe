7e2851e4e19269ee06cd7b8484b69c47ff091272
Lucas André <lucas.cancado89@gmail.com>
2022-02-06 17:40:18 -0300

    req5 done

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,25 +1,30 @@
 from ting_file_management.file_management import txt_importer
-from ting_file_management.queue import Queue
+import sys
 
 
-queue_obj = Queue()
-
-
-def process(path_file, queue_obj):
-    for linha in queue_obj.queue:
+def process(path_file, instance):
+    for linha in instance.queue:
         if linha['nome_do_arquivo'] == path_file:
             return
     dictio = {}
     dictio['nome_do_arquivo'] = path_file
     dictio['qtd_linhas'] = len(txt_importer(path_file))
     dictio['linhas_do_arquivo'] = txt_importer(path_file)
-    queue_obj.enqueue(dictio)
+    instance.enqueue(dictio)
     print(dictio)
 
 
 def remove(instance):
-    """Aqui irá sua implementação"""
+    if instance.__len__() == 0:
+        return print('Não há elementos')
+    removed_file = instance.dequeue()
+    print(removed_file)
+    # print(f'Arquivo {removed_file['nome_do_arquivo']} removido com sucesso')
 
 
 def file_metadata(instance, position):
-    """Aqui irá sua implementação"""
+    try:
+        item = instance.search(position)
+        print(item)
+    except IndexError:
+        return sys.stderr.write('Posição inválida')
