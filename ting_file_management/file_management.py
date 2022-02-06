5e261a8bd342be0ce8f92f518ae7c76447899cce
Luciano Amâncio <madruga665@hotmail.com>
2022-02-06 12:26:12 -0300

    requisito 2.2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,4 +1,10 @@
+import sys
+
+
 def txt_importer(path_file):
-    with open(path_file, 'r') as file:
-        text_list = file.read().splitlines()
-    return text_list
+    try:
+        with open(path_file, 'r') as file:
+            text_list = file.read().splitlines()
+        return text_list
+    except FileNotFoundError:
+        return sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
