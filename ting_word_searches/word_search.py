edec3a9012e0ebbf9266373f3c1f6535ad920f8a
igson <igson660@gmail.com>
2022-02-06 18:25:31 -0500

    quesito 06

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,18 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    data_list = list()
+    for index in instance.queue:
+        line = index['linhas_do_arquivo']
+        data = {
+            'palavra': word,
+            'arquivo': index['nome_do_arquivo'],
+            'ocorrencias': [],
+        }
+        for index in range(len(line)):
+            if word.upper() in line[index].upper():
+                data['ocorrencias'].append({'linha': index + 1})
+        if 0 < len(data['ocorrencias']):
+            data_list.append(data)
+    return data_list
 
 
 def search_by_word(word, instance):
