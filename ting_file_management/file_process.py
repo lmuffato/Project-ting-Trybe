032bcc5859992befce1d92d2bd63a3fcd1acfca5
Bruno Mendes <mendessbruno@gmail.com>
2022-02-07 19:24:12 -0300

    resolvido desafio 3

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,6 +1,24 @@
-def process(path_file, instance):
-    """Aqui irá sua implementação"""
+import sys
+from ting_file_management.file_management import txt_importer
 
+
+def process(path_file, instance):
+    linhas = txt_importer(path_file)
+    qtd = len(linhas)
+    output = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": qtd,
+        "linhas_do_arquivo": linhas
+    }
+
+    try:
+        existing = instance.search(0)
+        if existing['nome_do_arquivo'] != path_file:
+            instance.enqueue(output)
+            print(output)
+    except IndexError:
+        instance.enqueue(output)
+        print(output)
 
 def remove(instance):
     """Aqui irá sua implementação"""
