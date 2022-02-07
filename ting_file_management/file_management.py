e1bca063a4659e3a648267d3bf70dd91edf33227
Marcelofcdantas <65692996+Marcelofcd@users.noreply.github.com>
2022-02-07 18:37:29 -0300

    feat: requisito 2 finalizado

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -6,5 +6,7 @@ def txt_importer(path_file):
     try:
         if not path_file.endswith('txt'):
             print('Formato inválido', file=sys.stderr)
+        with open(path_file, 'r') as file:
+            return [line.replace('\n', '') for line in file]
     except FileNotFoundError:
         print(f'Arquivo {path_file} não encontrado', file=sys.stderr)
