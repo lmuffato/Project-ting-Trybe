5d5b1865e80f20747e5417ed3f6919f88ae5b1e2
Carlos Sá <carlos25xrm@gmail.com>
2022-02-07 11:02:06 -0300

    Push aleatório para tentar cancelar o avaliador que está rodando a mais de "Started 1d 13h 32m 4s ago"

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,7 @@
 def exists_word(word, instance):
     """Aqui irá sua implementação"""
+    # Push aleatório para tentar cancelar o avaliador que
+    # está rodando a mais de "Started 1d 13h 32m 4s ago"
 
 
 def search_by_word(word, instance):
