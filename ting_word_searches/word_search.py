6d6553de472b16759d0dec2a9b4e6e0def74eddf
JRGCast <joao.ralph1@gmail.com>
2022-02-07 13:08:39 -0300

    ah finalmente, sera?

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,37 @@
 def exists_word(word, instance):
     """Aqui irá sua implementação"""
+    result = []
+    for i in range(instance.__len__()):
+        item = instance.search(i)
+        the_dict = {
+            'palavra': word,
+            'arquivo': item['nome_do_arquivo'],
+            'ocorrencias': []
+        }
+        for index in range(item['qtd_linhas']):
+            if word.lower() in item['linhas_do_arquivo'][index].lower():
+                the_dict['ocorrencias'].append({'linha': index + 1})
+        if len(the_dict['ocorrencias']) > 0:
+            result.append(the_dict)
+    return result
 
 
 def search_by_word(word, instance):
     """Aqui irá sua implementação"""
+    result = []
+    for i in range(instance.__len__()):
+        item = instance.search(i)
+        the_dict = {
+            'palavra': word,
+            'arquivo': item['nome_do_arquivo'],
+            'ocorrencias': []
+        }
+        for index in range(item['qtd_linhas']):
+            if word.lower() in item['linhas_do_arquivo'][index].lower():
+                the_dict['ocorrencias'].append({
+                    'linha': index + 1,
+                    'conteudo': item['linhas_do_arquivo'][index]
+                })
+        if len(the_dict['ocorrencias']) > 0:
+            result.append(the_dict)
+    return result
