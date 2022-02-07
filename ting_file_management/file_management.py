e31fc54b33a01356ea470db7632c0149cf3a611e
viniciusfrancas019 <viniciusfrancas019@gmail.com>
2022-02-06 22:18:52 -0300

    add txt_importer function

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,19 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação """
+    """sobre o stderr
+    https://pythonguides.com/python-stderr-stdin-and-stdout/
+    https://www.geeksforgeeks.org/how-to-print-to-stderr-and-stdout-in-python/
+    """
+
+    file_extension = path_file[len(path_file) - 4:len(path_file)]
+
+    if file_extension != '.txt':
+        return sys.stderr.write('Formato inválido\n')
+
+    try:
+        file_to_read = open(path_file, "r").read()
+        return file_to_read.split("\n")
+    except FileNotFoundError:
+        sys.stderr.write(f'Arquivo {path_file} não encontrado\n')
