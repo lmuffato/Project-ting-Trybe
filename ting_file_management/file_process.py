a5e51659ba7b1c549c612501fd5a0fe4005b3ec4
viniciusfrancas019 <viniciusfrancas019@gmail.com>
2022-02-06 22:57:19 -0300

    add process logic

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,9 +1,20 @@
+import sys
 from ting_file_management.file_management import txt_importer
 from ting_file_management.util import get_files_name
 
 
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    # RabbitMQ feels
+    if path_file in get_files_name(instance):
+        return None
+    readed_file = txt_importer(path_file)
+    msg = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(readed_file),
+        "linhas_do_arquivo": readed_file
+    }
+    instance.enqueue(msg)
+    sys.stdout.write(str(msg))
 
 
 def remove(instance):
