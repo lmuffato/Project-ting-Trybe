f27e337fecc385eb74717d18ae23a476844e92b1
PollyanaOliveira <pollyana.oliveir@gmail.com>
2022-02-08 18:14:18 -0400

    feat: :sparkles: req6

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,22 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    result = []
+
+    for data in instance.data:
+        possible = []
+        result.append({
+            'palavra': word,
+            'arquivo': data['nome_do_arquivo'],
+            'ocorrencias': possible
+        })
+        
+        for index, line in enumerate(data['linhas_do_arquivo']):
+            if word in line:
+                possible.append({'linha': index + 1})
+        if not len(possible):
+            result.pop()
+
+    return result
+
 
 
 def search_by_word(word, instance):
