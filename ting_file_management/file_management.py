850a98bfefbe514e00e8dd5dcfc583f219ee43f3
Flavio Franco <flavio.win@gmail.com>
2022-02-08 08:57:09 -0300

    req2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,13 @@
+import sys
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if path_file.split('.')[1] != 'txt':
+        return sys.stderr.write("Formato inválido")
+
+    try:
+        with open(path_file, "r") as file:
+            my_file = file.read()
+            return my_file.split('\n')
+    except:
+        return sys.stderr.write(f"Arquivo {path_file} não encontrado")
+
