d23857ff93362e7760c15485f33c1f69fc834178
Flavio Franco <flavio.win@gmail.com>
2022-02-08 16:35:55 -0300

    corrigindo extensao txt

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -4,11 +4,11 @@ from xml.dom import NotFoundErr
 
 def txt_importer(path_file):
     if not path_file.endswith('.txt'):
-        sys.stderr.write("Formato inválido\n")
+        return sys.stderr.write("Formato inválido\n")
 
     try:
         with open(path_file, "r") as file:
             my_file = file.read()
             return my_file.split('\n')
     except NotFoundErr:
-        sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
+        return sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
