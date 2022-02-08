250471488f58b9e73f4cc3836c564d644d0b7455
Eduardo Costa Maciel <costaeduardomaciel@gmail.com>
2022-02-08 10:31:35 -0300

    Feat: Implementa funçao 'process'

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,21 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    if path_file in [key["nome_do_arquivo"] for key in instance.queue_value]:
+        return None
+
+    data = txt_importer(path_file)
+
+    result = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(data),
+        "linhas_do_arquivo": data,
+    }
+
+    instance.enqueue(result)
+    return sys.stdout.write(str(result))
 
 
 def remove(instance):
