39a7ee1005f656bdc3f4318f373aaab5c985e5cf
thalysmcarrara <thalysmatiascarrara@gmail.com>
2022-02-08 09:59:09 -0400

    requirement 1

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -1,15 +1,28 @@
 class Queue:
     def __init__(self):
-        """Inicialize sua estrutura aqui"""
+        self._data = list()
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        return len(self._data)
+
+    def __str__(self):
+        str_items = ""
+        for i in range(self.__len__()):
+            value = self._data[i]
+            str_items += str(value)
+            if i + 1 < self.__len__():
+                str_items += ", "
+
+        return "Queue(" + str_items + ")"
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        self._data.append(value)
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        return self._data.pop(0)
 
     def search(self, index):
-        """Aqui irá sua implementação"""
+        if 0 <= index < self.__len__():
+            return self._data[index]
+
+        raise IndexError
