b86e02003efecbe2970d788db63f112aa6339ce5
PollyanaOliveira <pollyana.oliveir@gmail.com>
2022-02-08 17:33:05 -0400

    feat: :sparkles: req3

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,18 @@
+from .file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    if any(file['nome_do_arquivo'] == path_file for file in instance.data):
+        return None
+
+    content = txt_importer(path_file)
+    data = {
+        'nome_do_arquivo': path_file,
+        'qtd_linhas': len(content),
+        'linhas_do_arquivo': content,
+    }
+    instance.enqueue(data)
+    return print(data)
 
 
 def remove(instance):
