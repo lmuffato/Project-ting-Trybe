5f4147b18f59e592aaa757fcf080a580b744a8b0
Tiago Santos <tiago881@gmail.com>
2022-02-08 14:29:10 -0300

    feat(requirement 2): function txt_importer

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,16 @@
+import sys
+
+
 def txt_importer(path_file):
-    pass
+    if not path_file.endswith("txt"):
+        print("Formato inválido", file=sys.stderr)
+
+    try:
+        with open(path_file, "r") as file:
+            result = file.read().split("\n")
+            return result
+    except FileNotFoundError:
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
+
+
+# https://www.delftstack.com/pt/howto/python/python-print-to-stderr/
