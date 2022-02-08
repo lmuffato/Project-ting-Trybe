8dd1fb886fa5bc971fa518f74781014fbe00baaa
Flavio Franco <flavio.win@gmail.com>
2022-02-08 10:09:11 -0300

    req3

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,22 @@
+import imp
+from file_management import txt_importer
+from queue import Queue
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    array_file = txt_importer(path_file)
+    for item in array_file:
+        instance.enqueue(item)
+    
+    return {
+        "nome_do_arquivo": path_file.split("/")[-1],
+        "qtd_linhas": len(instance),
+        "linhas_do_arquivo": array_file,
+    }
+
+""" queue = Queue()
+x = process("dev-requirements.txt", queue)
+print(x) """
+
 
 
 def remove(instance):
