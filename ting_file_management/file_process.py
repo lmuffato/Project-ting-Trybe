bbeeb9c9104050c99d78966b52adaf7de2aba1b4
Tiago Santos <tiago881@gmail.com>
2022-02-08 17:29:00 -0300

    feat(requirement 3): function process

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,19 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    for indice in range(len(instance)):
+        if instance.search(indice)["nome_do_arquivo"] == path_file:
+            return None
+    file_txt = txt_importer(path_file)
+    result = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(file_txt),
+        "linhas_do_arquivo": file_txt,
+    }
+    instance.enqueue(result)
+    sys.stdout.write(str(result))
 
 
 def remove(instance):
