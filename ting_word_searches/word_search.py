1be8d870c2392d04dd8477976cc0df4844662259
Fernando Castello Branco <fernandocastellob@gmail.com>
2022-02-08 17:32:17 -0300

    FEAT:Req06 - primeira tentativa

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,12 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    filter_word = []
+    data = instance.search(0)
+    line = list(data["linhas_do_arquivo"])
+
+    filter_word = list(filter(lambda el: word in el, line))
+    if len(filter_word) == 0:
+        return []
+    return filter_word
 
 
 def search_by_word(word, instance):
