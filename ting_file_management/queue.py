9b70bc1992b582e5a239704beaea99380d6d4719
Maria Baeta <m.martinsdg@gmail.com>
2022-02-07 21:45:53 -0300

    feat: :sparkles: Requisito 1

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -1,16 +1,28 @@
+# https://pythonhelp.wordpress.com/2012/09/25/filas-e-pilhas-em-python/
+
+
 class Queue:
     # Requisito 1
     def __init__(self):
         """Inicialize sua estrutura aqui"""
+        self.data = []
 
     def __len__(self):
         """Aqui irá sua implementação"""
+        self.__length = len(self.data)
+        return self.__length
 
     def enqueue(self, value):
         """Aqui irá sua implementação"""
+        self.data.append(value)
 
     def dequeue(self):
         """Aqui irá sua implementação"""
+        return self.data.pop(0)
 
     def search(self, index):
         """Aqui irá sua implementação"""
+        if index < 0:
+            raise IndexError
+        else:
+            return self.data[index]
