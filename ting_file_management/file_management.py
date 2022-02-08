677fc01893a68ba2781a141aa4270b18cad3c42f
Elisa <elisafranca.carvalho@gmail.com>
2022-02-07 22:34:44 -0300

    feat: implements 2nd requirement

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,17 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if not path_file.endswith('.txt'):
+        # Fonte: https://docs.python.org/3/library/sys.html
+        # deve ser exibida uma mensagem: Formato inválido
+        sys.stderr.write('Formato inválido\n')
+
+    try:
+        # read
+        with open(path_file, 'r') as data:
+            return data.read().split('\n')
+
+    except FileNotFoundError:
+        #  deve ser exibida a mensagem: Arquivo {path_file} não encontrado
+        sys.stderr.write(f'Arquivo {path_file} não encontrado\n')
