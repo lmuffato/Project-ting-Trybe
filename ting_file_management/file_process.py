16db81e7878cfd31bfe4b597112777f25ca39d1a
FIXsartoreto <fixsartoreto@gmail.com>
2022-02-08 08:44:55 -0300

    Requisito 4

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,10 +1,13 @@
 import sys
 from ting_file_management.file_management import txt_importer
 
+list = []
+
 
 def process(path_file, instance):
     if instance.__len__() == 0:
         file = txt_importer(path_file)
+        list.append(path_file)
 
         file_name = f"'nome_do_arquivo': '{path_file}'"
         line_count = f"'qtd_linhas': {len(file)}"
@@ -20,7 +23,13 @@ def process(path_file, instance):
 
 
 def remove(instance):
-    """Aqui irá sua implementação"""
+    if instance.__len__() == 0:
+        sys.stdout.write('Não há elementos\n')
+    else:
+        instance.dequeue()
+        path_name = list[0]
+        list.remove(path_name)
+        sys.stdout.write(f'Arquivo {path_name} removido com sucesso\n')
 
 
 def file_metadata(instance, position):
