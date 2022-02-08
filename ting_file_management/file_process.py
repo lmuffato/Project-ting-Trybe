f0895d5171f5140037381dc6df9007717e211060
FIXsartoreto <fixsartoreto@gmail.com>
2022-02-08 08:42:30 -0300

    Requisito 3

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,22 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    if instance.__len__() == 0:
+        file = txt_importer(path_file)
+
+        file_name = f"'nome_do_arquivo': '{path_file}'"
+        line_count = f"'qtd_linhas': {len(file)}"
+        all_lines = f"'linhas_do_arquivo': {file}"
+
+        sys.stdout.write(f'{file_name}')
+        sys.stdout.write(f'{line_count}')
+        sys.stdout.write(f'{all_lines}')
+
+        instance.enqueue(path_file)
+    else:
+        pass
 
 
 def remove(instance):
