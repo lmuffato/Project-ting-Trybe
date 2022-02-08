fe7a866f60413fbfc8f0a841ab9ea50d403fc4fb
Flavio Franco <flavio.win@gmail.com>
2022-02-08 16:25:57 -0300

    req7

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -3,12 +3,12 @@ from xml.dom import NotFoundErr
 
 
 def txt_importer(path_file):
-    if path_file.split('.')[1] != 'txt':
-        return sys.stderr.write("Formato inválido\n")
+    if not path_file.endswith('txt'):
+        sys.stderr.write("Formato inválido\n")
 
     try:
         with open(path_file, "r") as file:
             my_file = file.read()
             return my_file.split('\n')
     except NotFoundErr:
-        return sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
+        sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,35 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    data = []
+    for i in range(len(instance)):
+        data.append({
+            "palavra": word,
+            "arquivo": "",
+            "ocorrencias": [],
+        })
+        file = instance.search(i)
+        for j, item in enumerate(file["linhas_do_arquivo"]):
+            if word.lower() in item.lower():
+                data[i]["arquivo"] = file["nome_do_arquivo"]
+                data[i]["ocorrencias"].append({"linha": j + 1})
+
+    return [item for item in data if item["arquivo"] != ""]
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    data = []
+    for i in range(len(instance)):
+        data.append({
+            "palavra": word,
+            "arquivo": "",
+            "ocorrencias": [],
+        })
+        file = instance.search(i)
+        for j, item in enumerate(file["linhas_do_arquivo"]):
+            if word.lower() in item.lower():
+                data[i]["arquivo"] = file["nome_do_arquivo"]
+                data[i]["ocorrencias"].append({
+                    "linha": j + 1,
+                    "conteudo": item,
+                    })
+
+    return [item for item in data if item["arquivo"] != ""]
