d9403480baa77ea60f97a3ed38b342fa23bae3ba
Eduardo Costa Maciel <costaeduardomaciel@gmail.com>
2022-02-08 19:46:09 -0300

    Feat: Implementa funçao 'search_by_word'

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -33,4 +33,26 @@ def exists_word(word, instance):
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    word_occurrences = []
+
+    for line in instance.queue_value:
+        lines_file = file_lines(word, line["linhas_do_arquivo"])
+
+        if not len(lines_file):
+            return lines_file
+
+        for line_name in lines_file:
+            index_line = line["linhas_do_arquivo"].index(line_name) + 1
+
+            word_occurrences.append({
+                "linha": index_line,
+                "conteudo": line["linhas_do_arquivo"][index_line - 1]
+            })
+
+        return [
+            {
+                "palavra": word,
+                "arquivo": line["nome_do_arquivo"],
+                "ocorrencias": word_occurrences,
+            }
+        ]
