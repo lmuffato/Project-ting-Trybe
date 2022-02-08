21c0fd6f9760623f8947a87663d9b79ed157f154
PollyanaOliveira <pollyana.oliveir@gmail.com>
2022-02-08 17:27:00 -0400

    feat: :sparkles: req2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,11 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if not path_file.endswith('txt'):
+        return sys.stderr.write('Formato inválido\n')
+    try:
+        with open(path_file) as file:
+            return [line.rstrip() for line in file]
+    except FileNotFoundError:
+        return sys.stderr.write(f'Arquivo {path_file} não encontrado\n')
