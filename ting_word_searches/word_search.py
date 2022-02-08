73040317f24123f2ec120847d8de6f4a3c6e0ca9
w-cypriano <profwellington17@gmail.com>
2022-02-08 10:02:41 -0300

    req6

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,25 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação #vqv"""
+    list_index = []
+    teste = []
+    """result = [
+        line for line in instance.fila["linhas_do_arquivo"] if word in line
+    ]"""
+    for line in instance.fila:
+
+        if word in line["linhas_do_arquivo"]:
+            teste.append(line)
+        else:
+            return teste
+    for line in teste:
+        index = instance.fila["linhas_do_arquivo"].index(line)
+        list_index.append({"linha": index})
+        return [
+            {
+                "palavra": word,
+                "arquivo": instance.fila["nome_do_arquivo"],
+                "ocorrencias": list_index,
+            }
+        ]
 
 
 def search_by_word(word, instance):
