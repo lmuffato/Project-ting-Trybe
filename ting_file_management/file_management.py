edeb3354beac3b3a10f659aa5217dacad0afd9a7
Debora <deborinhapassos@gmail.com>
2022-02-08 18:03:48 -0300

    Req 1

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,20 @@
+import sys
+from os.path import exists
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if (path_file[-3:] != 'txt'):
+        print('Formato inválido', file=sys.stderr)
+
+    if not exists(path_file):
+        return print(f'Arquivo {path_file} não encontrado', file=sys.stderr)
+
+    with open(path_file, mode='r') as arch:
+        line = arch.read().splitlines()
+        return line
+
+
+# https://www.kite.com/python/
+# answers/how-to-read-a-newline-delimited-text-file-in-python
+
+# https://www.delftstack.com/pt/howto/python/python-print-to-stderr
