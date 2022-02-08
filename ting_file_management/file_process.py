1c8c4bc92010cfd880ec8df94991dade45be7fb1
Flavio Franco <flavio.win@gmail.com>
2022-02-08 11:25:43 -0300

    req5

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,28 +1,41 @@
-import imp
-from file_management import txt_importer
+from ting_file_management.file_management import txt_importer
 from queue import Queue
+import sys
 
 def process(path_file, instance):
-    file_name = path_file.split("/")[-1]
-
-    for i, _ in enumerate(instance):
-        if instance.search(i)["nome_do_arquivo"] == file_name:
+    for i in range(len(instance)):
+        if instance.search(i)["nome_do_arquivo"] == path_file:
             return None
 
     array_file = txt_importer(path_file)
 
     data = {
-        "nome_do_arquivo": file_name,
+        "nome_do_arquivo": path_file,
         "qtd_linhas": len(array_file),
         "linhas_do_arquivo": array_file,
     }
 
     instance.enqueue(data)
 
+    sys.stdout.write(str(data))
+
 
 def remove(instance):
-    """Aqui irá sua implementação"""
+    item_removed = instance.dequeue()
+
+    if not item_removed:
+        return sys.stdout.write("Não há elementos\n")
+
+    file_name = item_removed["nome_do_arquivo"]
+    
+    sys.stdout.write(f"Arquivo {file_name} removido com sucesso\n")
+
 
 
 def file_metadata(instance, position):
-    """Aqui irá sua implementação"""
+    try:
+        info = instance.search(position)
+        sys.stdout.write(str(info))
+    except:
+        sys.stderr.write("Posição inválida\n")
+
