ab2a4757c0d9a954d911769eb7cb77efaf8874ad
Elisa <elisafranca.carvalho@gmail.com>
2022-02-07 22:05:09 -0300

    feat: implements 1st requirement

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -1,15 +1,17 @@
 class Queue:
     def __init__(self):
-        """Inicialize sua estrutura aqui"""
+        self.data = list()
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        return len(self.data)
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        self.data.append(value)
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        return self.data.pop(0)
 
     def search(self, index):
-        """Aqui irá sua implementação"""
+        if index < 0:
+            raise IndexError
+        return self.data[index]
