de6a5c25e199eb3ae43a9c9edfdc782ef4e2a204
Willian Alves <willian.alvesb65@gmail.com>
2022-02-09 15:11:31 -0300

    Feat: Req 1 - enqueue adiciona um valor a fila

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -1,12 +1,14 @@
 class Queue:
     def __init__(self):
-        """Inicialize sua estrutura aqui"""
+        self.len = 0
+        self.list_fifo = []
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        return self.len
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        self.list_fifo.append(value)
+        self.len += 1
 
     def dequeue(self):
         """Aqui irá sua implementação"""
