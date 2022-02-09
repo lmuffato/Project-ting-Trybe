26a989165bdec5ad4780a1fe7faeb45337bc1427
Eduardo Seije <eduardoseije@gmail.com>
2022-02-09 19:30:51 -0300

    Feat Req3 - implementando loop para verificação se o mesmo nome do arquivo é passado

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,10 +1,15 @@
+from importlib.resources import path
 import sys
 from ting_file_management.file_management import txt_importer
 
 
 def process(path_file, instance):
     """Aqui irá sua implementação"""
-
+    data = txt_importer(path_file)
+    for i in range(len(instance)):
+        if instance.search(i)["nome_do_arquivo"] == path_file:
+            return None
+   
 
 def remove(instance):
     """Aqui irá sua implementação"""
