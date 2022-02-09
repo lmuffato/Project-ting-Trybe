a76ddbc483bdba00a9bd58a769b19730a575075c
luanlsr <luanfswd@gmail.com>
2022-02-09 19:29:52 -0300

    req 6 finalizado

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,23 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    palavras_encontradas = []
+    for txt_file in instance.items:
+        data = {
+            "palavra": word,
+            "arquivo": txt_file["nome_do_arquivo"],
+            "ocorrencias": []
+        }
+        palavras_encontradas.append(data)
+
+    # https://realpython.com/python-enumerate/#unpacking-arguments-with-enumerate
+        for index, line in enumerate(txt_file["linhas_do_arquivo"]):
+            current_line = index + 1
+            if word.lower() in line.lower():
+                palavras_encontradas[index]["ocorrencias"].append({
+                    "linha": current_line
+                })
+                if len(palavras_encontradas[index]["ocorrencias"]) > 0:
+                    return palavras_encontradas
+        return list()
 
 
 def search_by_word(word, instance):
