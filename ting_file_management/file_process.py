54eb36309d50ba04545f3dc9176ddb0781d383e0
Tiago Santos <tiago881@gmail.com>
2022-02-09 17:18:07 -0300

    feat(requirement 4): function remove

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -17,7 +17,10 @@ def process(path_file, instance):
 
 
 def remove(instance):
-    """Aqui irá sua implementação"""
+    element = instance.dequeue()
+    if element:
+        print(f"Arquivo {element['nome_do_arquivo']} removido com sucesso")
+    print("Não há elementos")
 
 
 def file_metadata(instance, position):
