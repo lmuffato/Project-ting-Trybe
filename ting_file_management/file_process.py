4a49d2c4c1dfb21a336e0cc1f7742e5952277f0b
Willian Alves <willian.alvesb65@gmail.com>
2022-02-09 16:33:04 -0300

    Feat: req 3- process ignora ou retorna mensagem

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,19 @@
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    for i in range(len(instance)):
+        if path_file == instance.search(i)['nome_do_arquivo']:
+            return None
+
+    read = txt_importer(path_file)
+    mensagem = {
+        'nome_do_arquivo': path_file,
+        'qtd_linhas': len(read),
+        'linhas_do_arquivo': read
+    }
+    instance.enqueue(mensagem)
+    print(mensagem)
 
 
 def remove(instance):
