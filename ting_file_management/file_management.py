8c4430f6e760fd24fe88f43681b9d570fcc2e971
Eduardo Seije <eduardoseije@gmail.com>
2022-02-09 18:45:53 -0300

    Feat Req2 - implementação do método para ler abrir o arquivo no modo de leitura e adicionar a quebra de linha pela estrutura de repetição

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -5,3 +5,8 @@ def txt_importer(path_file):
     """Aqui irá sua implementação"""
     if not path_file.endswith(".txt"):
         sys.stderr.write("Formato inválido\n")
+    try:
+        with open(path_file, 'r') as file:
+            return [news.replace('\n', '') for news in file]
+    except FileNotFoundError:
+        sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
