470ebe3047ba38a23c6df7277c6ef01850d9c3ac
Daniel Ribeiro <neto.daniribeiro@gmail.com>
2022-02-09 09:02:52 -0300

    Requisito 06: word_search

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,33 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    processed_files_list = []
+
+    for index in range(len(instance)):
+        file_content = instance.search(index)
+
+        file_name = file_content["nome_do_arquivo"]
+
+        file_lines = file_content["linhas_do_arquivo"]
+
+        process_lines = []
+
+        for line in file_lines:
+            if word.lower() in line:
+                process_lines.append(
+                    {{"linha": file_lines.index(line), "conteudo": line}}
+                )
+
+        if len(process_lines) == 0:
+            return []
+
+        processed_files_list.append(
+            {
+                "palavra": word,
+                "arquivo": file_name,
+                "ocorrencias": process_lines,
+            }
+        )
+
+    return processed_files_list
 
 
 def search_by_word(word, instance):
