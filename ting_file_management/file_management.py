c68a53aee757f29a5ae6fd07292faa33b05615be
rafael <rafittu@outlook.com>
2022-02-09 14:43:52 -0300

    feat req 2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,12 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if not path_file.endswith(".txt"):
+        return sys.stderr.write('Formato inválido\n')
+
+    try:
+        txt_file = open(path_file, "r").read().splitlines()
+        return txt_file
+    except FileNotFoundError:
+        sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
