8e77459dd194479310ff17bcf2e50ccadb3db832
biazidioti <bzferreira.bio@gmail.com>
2022-02-09 18:55:56 -0300

    req 1

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -1,15 +1,18 @@
 class Queue:
     def __init__(self):
-        """Inicialize aqui"""
+        self.list = list()
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        return len(self.list)
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        return self.list.append(value)
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        return self.list.pop(0)
 
     def search(self, index):
-        """Aqui irá sua implementação"""
+        if index >= 0:
+            return self.queue[index]
+        else:
+            raise IndexError
