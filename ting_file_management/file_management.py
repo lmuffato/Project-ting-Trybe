97bf2d226353dc1470b7ae85cbd7cee8eaad9730
Patrick Morais <ppternunes@gmail.com>
2022-02-09 15:07:19 -0300

    req 2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,13 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implemewwntação"""
+    stderr = sys.stderr
+    if not path_file.endswith(".txt"):
+        print("Formato inválido", file=stderr)
+    try:
+        with open(path_file, mode="r") as tfile:
+            content = tfile.read()
+            return content.split("\n")
+    except FileNotFoundError:
+        print(f"Arquivo {path_file} não encontrado", file=stderr)
