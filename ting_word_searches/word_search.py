fec6f31403897ec90287324049be432f222ecbd0
guidugaich <guidugaich.dev@gmail.com>
2022-02-09 15:54:04 -0300

    req 6 & 7 - word search

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,41 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    found_words = []
+    for entry in instance._data:
+        result = {
+            'palavra': word,
+            'arquivo': entry['nome_do_arquivo'],
+            'ocorrencias': []
+        }
+
+        for index, line in enumerate(entry['linhas_do_arquivo']):
+            line = line.replace('.', '')
+            if word.lower() in line.lower().split():
+                result['ocorrencias'].append({'linha': index + 1})
+
+        if len(result['ocorrencias']) > 0:
+            found_words.append(result)
+
+    return found_words
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    found_sentences = []
+    for entry in instance._data:
+        result = {
+            'palavra': word,
+            'arquivo': entry['nome_do_arquivo'],
+            'ocorrencias': []
+        }
+
+        for index, line in enumerate(entry['linhas_do_arquivo']):
+            line_minus_period = line.replace('.', '')
+            if word.lower() in line_minus_period.lower().split():
+                result['ocorrencias'].append({
+                    'linha': index + 1,
+                    'conteudo': line
+                })
+
+        if len(result['ocorrencias']) > 0:
+            found_sentences.append(result)
+
+    return found_sentences
