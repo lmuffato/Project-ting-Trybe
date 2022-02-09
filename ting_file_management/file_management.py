17b543ecb5f920a2a79674c5a89de6fd3627b0b6
Parker9309 <juan.tentiomaru9309@gmail.com>
2022-02-09 14:22:11 -0300

    feat: req 1 && req 2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,14 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if path_file.endswith(".txt"):
+        try:
+            txt_file = open(path_file, "r")
+            return txt_file.read().splitlines()
+        except FileNotFoundError:
+            sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
+    else:
+        sys.stderr.write("Formato inválido\n")
+
+        # https://stackoverflow.com/questions/5574702/how-to-print-to-stderr-in-python
--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -1,15 +1,23 @@
+from collections import deque
+
+
 class Queue:
     def __init__(self):
-        """Inicialize sua estrutura aqui"""
+        self.queue = deque()
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        return len(self.queue)
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        self.queue.append(value)
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        return (
+            self.queue.popleft()
+        )  # https://pythontic.com/containers/deque/popleft
 
     def search(self, index):
-        """Aqui irá sua implementação"""
+        if 0 <= index <= self.__len__() - 1:
+            return self.queue[index]
+        else:
+            raise IndexError
