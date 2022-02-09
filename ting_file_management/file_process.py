6823b49366b2623bea3ba6457fa69c9fb4964d0c
rafael <rafittu@outlook.com>
2022-02-09 15:06:49 -0300

    feat req 3

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,17 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    if path_file not in instance.queue:
+        txt_file = txt_importer(path_file)
+        instance.enqueue(path_file)
+        data = {
+            "nome_do_arquivo": path_file,
+            "qtd_linhas": len(txt_file),
+            "linhas_do_arquivo": txt_file
+        }
+        return sys.stdout.write(f"{data}")
 
 
 def remove(instance):
