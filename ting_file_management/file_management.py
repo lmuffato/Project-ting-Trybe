7ea5df57aeab151d17f48099bb282d9d3127ac34
Lucas Machado <lucas.almeida.machado92@gmail.com>
2022-02-09 18:32:16 -0300

    Req 2.1 - retorno da lista contendo as linhas do arquivo

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,10 @@
+# https://pt.stackoverflow.com/questions/226182/quebra-de-linha-em-um-arquivo-txt
+# o link acima serviu para ter conhecimento da função splitlines()
+
 def txt_importer(path_file):
     """Aqui irá sua implementação"""
+    if path_file.endswith('.txt'):
+        txt_file = open(path_file, "r").read().splitlines()
+        return list(txt_file)
+    else:
+        print("Arquivo" + {path_file} + "não encontrado")
