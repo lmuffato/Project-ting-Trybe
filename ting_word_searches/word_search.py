598829f1c8535aff447c1f9be9de66b63c386216
Elisa <elisafranca.carvalho@gmail.com>
2022-02-08 22:09:51 -0300

    feat: implements 6th requirement 1st part

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,38 @@
+# retorno esperado:
+# [{
+#  "palavra": "de",
+#  "arquivo": "arquivo_teste.txt",
+#  "ocorrencias": [
+#    {
+#      "linha": 1
+#    },
+#    {
+#      "linha": 2
+#    }
+#  ]
+# }]
+
+
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    returnList = []
+
+    for item in instance.data:
+        events = []
+        returnList.append({
+            'palavra': word,
+            'arquivo': item['nome_do_arquivo'],
+            'ocorrencias': events
+        })
+        # Fonte: https://realpython.com/python-enumerate/
+        for i, line in enumerate(item['linhas_do_arquivo']):
+            if word in line:
+                events.append({
+                    'linha': i + 1
+                })
+        if not len(events):
+            returnList.pop()
+
+    return returnList
 
 
 def search_by_word(word, instance):
