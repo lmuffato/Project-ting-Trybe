02d9dfa4cdde8d591b2918546921e0542780d77b
Willian Alves <willian.alvesb65@gmail.com>
2022-02-09 16:00:47 -0300

    Feat:req 2- txt_importer retorna lista, msg Arquivo invalodo, Msg format invalido

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,18 @@
+from functools import lru_cache
+import sys
+import os.path
+
+@lru_cache
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    formatFile = os.path.splitext(path_file)[1]
+    if(formatFile != '.txt'):
+        return sys.stderr.write('Formato inválido\n')
+    try:
+        with open(path_file, 'r', encoding='utf8') as file:
+            reader = file
+            lisText = [rows.strip('\n') for rows in reader]
+            return lisText
+    except FileNotFoundError:
+        return sys.stderr.write(
+            'Arquivo {} não encontrado\n'
+            .format(path_file))
