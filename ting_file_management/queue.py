c84989a5b4867400f0088ec1b4ed4306e115d78a
Lucas Machado <lucas.almeida.machado92@gmail.com>
2022-02-08 22:56:14 -0300

    Req 1.1 e 1.2 = enqueue e dequeue feitos

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -1,15 +1,24 @@
 class Queue:
     def __init__(self):
-        """Inicialize sua estrutura aqui - iniciando projeto"""
+        """Inicialize sua estrutura aqui"""
+        self._data = list()
 
     def __len__(self):
         """Aqui irá sua implementação"""
+        return len(self._data)
 
     def enqueue(self, value):
         """Aqui irá sua implementação"""
+        return self._data.append(value)
 
     def dequeue(self):
         """Aqui irá sua implementação"""
+        if self.__len__() is None:
+            return None
+
+        value = self._data[0]
+        del self._data[0]
+        return value
 
     def search(self, index):
         """Aqui irá sua implementação"""
