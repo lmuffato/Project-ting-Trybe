3f1316ac0b81825e7443778f89351f66139690b1
Tiago Santos <tiago881@gmail.com>
2022-02-09 19:00:15 -0300

    feat(requirement 5): function file_metadata

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -24,4 +24,8 @@ def remove(instance):
 
 
 def file_metadata(instance, position):
-    """Aqui irá sua implementação"""
+    try:
+        result = instance.search(position)
+        sys.stdout.write(str(result))
+    except IndexError:
+        sys.stderr.write("Posição inválida")
