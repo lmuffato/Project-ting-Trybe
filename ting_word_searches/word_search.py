068dbdd287f5e8484a981c250487a30319e52931
AndreBarroso <andre_barroso17@hotmail.com>
2022-02-09 12:54:34 -0300

    feat: requisito6

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,20 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    seach = list()
+    for index in range(instance.__len__()):
+        data_seach = instance.search(index)
+        data = {
+            'palavra': word,
+            'arquivo': data_seach['nome_do_arquivo'],
+            'ocorrencias': []
+        }
+        for count, line in enumerate(data_seach['linhas_do_arquivo']):
+            if word.upper() in line.upper():
+                data['ocorrencias'].append({
+                    'linha': count + 1
+                })
+        if len(data['ocorrencias']) > 0:
+            seach.append(data)
+    return seach
 
 
 def search_by_word(word, instance):
