db3e3bab2ad1e08901fd1f82f46840879904992d
guidugaich <guidugaich.dev@gmail.com>
2022-02-08 21:53:42 -0300

    req 1 - Queue

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -1,15 +1,23 @@
 class Queue:
     def __init__(self):
-        """Inicialize sua estrutura aqui"""
+        self._data = list()
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        return len(self._data)
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        self._data.append(value)
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        if self.__len__() == 0:
+            return None
+
+        dequeued_value = self._data[0]
+        self._data = self._data[1:]
+        return dequeued_value
 
     def search(self, index):
-        """Aqui irá sua implementação"""
+        if index < 0 or self.__len__() < index + 1:
+            raise IndexError
+
+        return self._data[index]
