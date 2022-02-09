6aac65de69fe357c0e868bc03915cb95f9a3a7f6
Lucas Machado <lucas.almeida.machado92@gmail.com>
2022-02-09 19:07:41 -0300

    Req 2 completo - criando as exceptions

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,10 +1,16 @@
 # https://pt.stackoverflow.com/questions/226182/quebra-de-linha-em-um-arquivo-txt
 # o link acima serviu para ter conhecimento da função splitlines()
+# https://www.delftstack.com/pt/howto/python/python-print-to-stderr/
+import sys
+
 
 def txt_importer(path_file):
     """Aqui irá sua implementação"""
-    if path_file.endswith('.txt'):
-        txt_file = open(path_file, "r").read().splitlines()
-        return list(txt_file)
-    else:
-        print("Arquivo" + {path_file} + "não encontrado")
+    try:
+        if path_file.endswith('.txt'):
+            txt_file = open(path_file, "r").read().splitlines()
+            return list(txt_file)
+        else:
+            print("Formato inválido", file=sys.stderr)
+    except IOError:
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
