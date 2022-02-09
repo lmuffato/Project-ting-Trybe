f63ddeb48eb2eaa28ad1f39958d6a4eb7d4537e8
Parker9309 <juan.tentiomaru9309@gmail.com>
2022-02-09 16:26:22 -0300

    feat: req 3 test

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,20 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    data = txt_importer(path_file)
+    dict = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(data),
+        "linhas_do_arquivo": data,
+    }
+    for index in instance.queue:
+        if dict["nome_do_arquivo"] == index["nome_do_arquivo"]:
+            return None
+    else:
+        instance.enqueue(dict)
+        sys.stdout.write(str(dict))
 
 
 def remove(instance):
