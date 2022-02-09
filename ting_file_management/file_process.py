f7d88fc1160aae7de8245122f4201e639ee1cd6c
Robertson <robertson.nunes@gmail.com>
2022-02-09 19:26:21 -0300

    feat req 3 - implements process function

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,22 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
     """Aqui irá sua implementação"""
+    lines = txt_importer(path_file)
+    if lines:
+        content = {
+            "nome_do_arquivo": path_file,
+            "qtd_linhas": len(lines),
+            "linhas_do_arquivo": lines,
+        }
+        for file in instance.queue:
+            if file["nome_do_arquivo"] == path_file:
+                return None
+
+        instance.enqueue(content)
+        sys.stdout.write(str(content))
 
 
 def remove(instance):
