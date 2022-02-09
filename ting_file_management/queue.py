166b38eaeee47fcf5342b3f1a1659b51e757fb2f
Eduardo Seije <eduardoseije@gmail.com>
2022-02-09 17:33:20 -0300

    Feat Req1 - implementação do método len, que retorna o tamanho de values

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -5,6 +5,7 @@ class Queue:
 
     def __len__(self):
         """Aqui irá sua implementação"""
+        return len(self.values)
 
     def enqueue(self, value):
         """Aqui irá sua implementação"""
