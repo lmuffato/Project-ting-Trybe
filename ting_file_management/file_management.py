93febefb2fb15e7faebf202bfdc353b1a0be5df5
Nathalia Zebral <natzebral@gmail.com>
2022-02-09 10:24:33 -0300

    implementa funcao txt_importer

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,19 @@
+# https://www.kite.com/python/answers/
+# how-to-read-a-newline-delimited-text-file-in-python
+# https://www.delftstack.com/pt/howto/python/
+# python-print-to-stderr
+import sys
+from os.path import exists
+
+
 def txt_importer(path_file):
     """Aqui irá sua implementação"""
+    if (path_file[-3:] != 'txt'):
+        print("Formato inválido", file=sys.stderr)
+
+    if not exists(path_file):
+        return print(f'Arquivo {path_file} não encontrado', file=sys.stderr)
+
+    with open(path_file, mode='r') as txt_file:
+        rows = txt_file.read().splitlines()
+        return rows
