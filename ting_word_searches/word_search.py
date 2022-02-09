b7d893ee4ca43496aea2539ce88458696860b7e1
Robertson <robertson.nunes@gmail.com>
2022-02-09 19:53:18 -0300

    feat req 6 - implements exists_word function

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,21 @@
 def exists_word(word, instance):
     """Aqui irá sua implementação"""
+    result = list()
+    for file in instance.queue:
+        content = {
+            "palavra": word,
+            "arquivo": file["nome_do_arquivo"],
+            "ocorrencias": [],
+        }
+        for index, line in enumerate(file["linhas_do_arquivo"]):
+            line_index = index + 1
+            word_lc = word.lower()
+            line_lc = line.lower()
+            if word_lc in line_lc:
+                content["ocorrencias"].append({"linha": line_index})
+        if content["ocorrencias"]:
+            result.append(content)
+    return result
 
 
 def search_by_word(word, instance):
