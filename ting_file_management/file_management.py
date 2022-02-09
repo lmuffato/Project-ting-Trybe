7282b83a0fcbb1b28da0ce3f46f8eb14ed0fc33c
NilsonRCS <nilson.ribeiro.souza3@gmail.com>
2022-02-09 20:33:04 -0300

    feat: requirement:02

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,19 @@
+import sys
+
+
+# endswith https://www.w3schools.com/python/ref_string_endswith.asp
+# sys.stderr.write()
+# https://stackoverflow.com/questions/5574702/how-to-print-to-stderr-in-python
+# + https://www.youtube.com/watch?v=5za6eRdHjpw
+# python split https://www.w3schools.com/python/ref_string_split.asp
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if not path_file.endswith(".txt"):
+        return sys.stderr.write("Formato inválido\n")
+    try:
+        with open(path_file, mode="r") as f:
+            data = f.read()
+            return data.split("\n")
+
+    except FileNotFoundError:
+        sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
