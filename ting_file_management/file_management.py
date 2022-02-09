b40a318b5f87e257d721e3be6b4fc71d3cf48383
Daniel Ribeiro <neto.daniribeiro@gmail.com>
2022-02-09 08:22:35 -0300

    Requisito 02: file_management

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,14 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if not path_file.endswith(".txt"):
+        sys.stderr.write("Formato inválido\n")
+    try:
+        file = open(path_file, "r")
+        file_content = file.read()
+        lines = file_content.split("\n")
+        file.close()
+        return lines
+    except FileNotFoundError:
+        sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
