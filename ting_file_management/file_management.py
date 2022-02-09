b24fbf1e08d2bd7d0c7268adf5a0183c7330c3cc
Robertson <robertson.nunes@gmail.com>
2022-02-09 18:52:24 -0300

    feat req 2 - implements txt_importer function

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,18 @@
+import sys
+
+
 def txt_importer(path_file):
     """Aqui irá sua implementação"""
+    if not path_file.endswith(".txt"):
+        # https://stackoverflow.com/questions/5574702/how-to-print-to-stderr-in-python
+        sys.stderr.write("Formato inválido\n")
+        return None
+
+    try:
+        with open(path_file) as file:
+            return [line.split("\n")[0] for line in file.readlines()]
+    except FileNotFoundError:
+        sys.stderr.write(
+            "Arquivo statics/arquivo_nao_existe.txt não encontrado\n"
+        )
+        return None
