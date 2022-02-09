c6a9d06b148c92e41d227d2c9423662c0b9b2c36
Mathaus Annibal <mathausannibal@gmail.com>
2022-02-09 20:14:30 -0300

    adiciona requisito 2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,13 @@
+import sys
+
+
 def txt_importer(path_file):
     """Aqui irá sua implementação"""
+    if not path_file.endswith(".txt"):
+        return sys.stderr.write("Formato inválido\n")
+    try:
+        with open(path_file, mode="r") as file:
+            arq = file.read().splitlines()
+            return arq
+    except FileNotFoundError:
+        sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
