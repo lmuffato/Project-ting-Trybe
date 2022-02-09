27362e724d64a52b3956bf7a72a1d1464c8cb6a4
guidugaich <guidugaich.dev@gmail.com>
2022-02-09 09:47:37 -0300

    req 2 - file management

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,12 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    try:
+        if not path_file.endswith('.txt'):
+            sys.stderr.write("Formato inválido\n")
+            return None
+        with open(path_file) as file:
+            return file.read().splitlines()
+    except FileNotFoundError:
+        sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
