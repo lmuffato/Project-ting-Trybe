df445e66d57b854834f2c2d5fc62e02e59431914
moraesvictor <moraesvictor@gmail.com>
2022-02-10 19:37:24 -0300

    feat: req 3

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,23 @@
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+      file_already_exists = next(
+        (
+            file
+            for file in instance.queue
+            if file["nome_do_arquivo"] == path_file
+        ),
+        False,
+    )
+    if file_already_exists:
+        return file_already_exists
+
+    file_data = txt_importer(path_file)
+    file_dict = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(file_data),
+        "linhas_do_arquivo": file_data,
+    }
+    instance.enqueue(file_dict)
+    print(file_dict)
 
 
 def remove(instance):
