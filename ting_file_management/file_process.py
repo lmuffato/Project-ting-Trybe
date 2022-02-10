e43684239574532bb19d486a6a9139a7914cb2c9
Orlando Flores <orlando_flores@id.uff.br>
2022-02-10 18:10:30 -0300

    feat: requisito 3

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,17 @@
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    content = txt_importer(path_file)
+    file_data = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(content),
+        "linhas_do_arquivo": content,
+    }
+
+    if len(instance) <= 0 or instance.search(0) != file_data:
+        instance.enqueue(file_data)
+        print(file_data)
 
 
 def remove(instance):
