97f94d9bd4fb7de0e38d188d49081a1356ad2961
Rmgreco <rafaelmgreco@hotmail.com>
2022-02-10 16:06:32 -0300

    2,3,4

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,13 @@
+import sys
+
+
 def txt_importer(path_file):
     """Aqui irá sua implementação"""
+    try:
+        if not str(path_file).endswith(".txt"):
+            return print("Formato inválido", file=sys.stderr)
+        with open(path_file) as file:
+            return file.read().splitlines()
+
+    except FileNotFoundError:
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,9 +1,33 @@
+import sys
+
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
     """Aqui irá sua implementação"""
+    file = txt_importer(path_file)
+    result = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(file),
+        "linhas_do_arquivo": file,
+    }
+    enqueued = False
+    for element in instance._data:
+        if element["nome_do_arquivo"] == path_file:
+            enqueued = True
+            break
+    if not enqueued:
+        instance.enqueue(result)
+    return print(result)
 
 
 def remove(instance):
     """Aqui irá sua implementação"""
+    if instance.__len__() == 0:
+        return print("Não há elementos")
+    file = instance._data[0]["nome_do_arquivo"]
+    instance.dequeue()
+    return print(f"Arquivo {file} removido com sucesso")
 
 
 def file_metadata(instance, position):
