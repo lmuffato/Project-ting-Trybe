03f1563c409752eb341489f057f80663381ca898
NilsonRCS <nilson.ribeiro.souza3@gmail.com>
2022-02-10 11:18:28 -0300

    feat: agradecimentos

---
+++ ting_file_management/agradecimentos.py
@@ -0,0 +1,5 @@
+# agradeço aos amigos
+# Lucas Lara, Vinicius rodrigues, carlos sá, rafael medeiros entre outros
+# por auxilio no projeto, estudar comigo, apoio mental
+# me incentivar e estar sempre dispostos a me ajudar durante todo esse ano
+# MARAVILHOOOOOOOOOOOOOSOOOOOOOOOOOOOO
