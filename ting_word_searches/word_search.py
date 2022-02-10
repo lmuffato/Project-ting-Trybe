03b88641f6051c33465b22efc0b7674da92f3516
moraesvictor <moraesvictor@gmail.com>
2022-02-10 20:36:37 -0300

    feat: req 6

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,21 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
-
+    report = []
+    occurrences = 0
+    for file in instance.queue:
+        file_report = {
+            "palavra": word,
+            "arquivo": file["nome_do_arquivo"],
+            "ocorrencias": [
+                {"linha": index + 1}
+                for index, linha in enumerate(file["linhas_do_arquivo"])
+                if word.lower() in linha.lower()
+            ],
+        }
+        occurrences += len(file_report["ocorrencias"])
+        report.append(file_report)
+    if not occurrences:
+        report = []
+    return report
 
 def search_by_word(word, instance):
     """Aqui irá sua implementação"""
