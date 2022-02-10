6d41c5f60c4a2411fee9d8fbe84cb4b09ff0c2a5
Willian Alves <willian.alvesb65@gmail.com>
2022-02-10 20:24:25 -0300

    Feat: req ull é o fimmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,35 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    itens = list()
+    for i in range(instance.__len__()):
+        readFile = instance.search(i)
+        occurrences = list()
+        for line, phrase in enumerate(readFile['linhas_do_arquivo'], start=1):
+            if word in phrase:
+                occurrences.append({'linha': line})
+        if len(occurrences):
+            itens.append({
+                'palavra': word,
+                'arquivo': readFile['nome_do_arquivo'],
+                'ocorrencias': occurrences
+            })
+    return itens
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    itens = list()
+    for i in range(instance.__len__()):
+        readFile = instance.search(i)
+        occurrences = list()
+        for line, phrase in enumerate(readFile['linhas_do_arquivo'], start=1):
+            if word.upper() in phrase.upper():
+                occurrences.append({
+                    'linha': line,
+                    'conteudo': phrase
+                })
+        if len(occurrences):
+            itens.append({
+                'palavra': word,
+                'arquivo': readFile['nome_do_arquivo'],
+                'ocorrencias': occurrences
+            })
+    return itens
