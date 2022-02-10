3ad31c58f441f653a0a2d26a88ebd2772153a315
biazidioti <bzferreira.bio@gmail.com>
2022-02-10 15:57:46 -0300

    defini funcao process

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,21 @@
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    file = txt_importer(path_file)
+    for index in range(len(instance)):
+        # ignora arquivos com mesmo nome
+        if instance.search(index)["nome_do_arquivo"] == path_file:
+            return None
+
+    data = {
+            "nome_do_arquivo": path_file,
+            "qtd_linhas": len(file),
+            "linhas_do_arquivo": (file),
+        }
+
+    instance.enqueue(data)
+    return print(data)
 
 
 def remove(instance):
