f577e9700f41e5bd7d3d469314e059ab1dbfdb3c
Marcelofcdantas <65692996+Marcelofcd@users.noreply.github.com>
2022-02-10 11:32:16 -0300

    feat: file_metadata criado

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,15 +1,21 @@
+import sys
+
+
 def process(path_file, instance):
     """Aqui irá sua implementação"""
-    # try:
-    #     with open(path_file, 'r') as file:
-    #         lines = (for line in file)
-    #         file_name = path_file
-    #         lines_number = (for line in file)
 
 
 def remove(instance):
-    """Aqui irá sua implementação"""
+    if len(instance) < 1:
+        return "Não há elementos"
+    result = instance.dequeue()
+    print(f"Arquivo {result['nome_do_arquivo']} removido com sucesso")
 
 
+# função inspirada na forma feita pelo João Andrade Jr
 def file_metadata(instance, position):
-    """Aqui irá sua implementação"""
+    if position > instance.__len__():
+        return sys.stderr.write("Posição inválida")
+    else:
+        data = instance.search(position)
+        print(data, sys.stdout)
