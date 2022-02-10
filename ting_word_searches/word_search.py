069b4bfaffa47c3d722a7cab4271543233d83f9b
William Willers <w.willers@outlook.com>
2022-02-10 20:13:12 -0300

    Testando em prod - exists_word

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,22 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    for index in range(len(instance)):
+        queueItem = instance.search(index)
+    data = {
+        "palavra": word,
+        "arquivo": queueItem["nome_do_arquivo"],
+        "ocorrencias": []
+    }
+    line = 0
+    for index in queueItem["nome_do_arquivo"]:
+        if word.lower() in index.lower():
+            line += 1
+            data["ocorrencias"].append({
+                "linha": line
+            })
+    items = list()
+    if len(data["ocorrencias"]) > 0:
+        items.append(data)
+    return items
 
 
 def search_by_word(word, instance):
