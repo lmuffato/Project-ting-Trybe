88bcf8088ae9d2cf96a8516857b9410d80890298
Felipe Shcolnik <shcolnik@gmail.com.br>
2022-02-10 17:53:25 -0300

    file not found

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -4,5 +4,8 @@ import sys
 def txt_importer(path_file):
     if not path_file[-3:] == 'txt':
         return sys.stderr.write('Formato inválido\n')
-    with open(path_file, 'r') as file:
-        return file.read().splitlines()
+    try:
+        with open(path_file, 'r') as file:
+            return file.read().splitlines()
+    except:
+        return sys.stderr.write("Arquivo statics/arquivo_nao_existe.txt não encontrado\n")
