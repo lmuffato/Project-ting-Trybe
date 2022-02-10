a1915a89dc2329a95e8143ad40265bd82fe787ca
moraesvictor <moraesvictor@gmail.com>
2022-02-10 19:29:37 -0300

    feat: req 2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,15 @@
+import sys
+from os import path
+
+
 def txt_importer(path_file):
     """Aqui irá sua implementação"""
+    if not path_file.endswith(".txt"):
+        return sys.stderr.write("Formato inválido\n")
+    if not path.exists(path_file):
+        return sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
+    with open(path_file) as file:
+        result = [line.rstrip("\n") for line in file.readlines()]
+        for line in file.readlines():
+            print(line)
+        return result
