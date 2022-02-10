aa37ec9e8cd238821cea7ecb49bb9060d5ad321d
Tiago Santos <tiago881@gmail.com>
2022-02-10 14:13:58 -0300

    feat(requirement 6): function exists_wordOC

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,18 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    is_exists = list()
+    for index in range(instance.__len__()):
+        result = instance.search(index)
+        data = {
+            "palavra": word,
+            "arquivo": result["nome_do_arquivo"],
+            "ocorrencias": [],
+        }
+        for increment, line in enumerate(result["linhas_do_arquivo"]):
+            if word.upper() in line.upper():
+                data["ocorrencias"].append({"linha": increment + 1})
+        if len(data["ocorrencias"]) > 0:
+            is_exists.append(data)
+    return is_exists
 
 
 def search_by_word(word, instance):
