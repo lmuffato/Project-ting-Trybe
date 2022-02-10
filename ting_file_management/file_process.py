2fa185bf20b4524c4f90b77258047a5dcf68d56e
Mathaus Annibal <mathausannibal@gmail.com>
2022-02-09 23:27:13 -0300

    adiciona requisito 4

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -17,6 +17,11 @@ def process(path_file, instance):
 
 def remove(instance):
     """Aqui irá sua implementação"""
+    if len(instance) > 0:
+        remove = instance.dequeue()["nome_do_arquivo"]
+        sys.stdout.write(f"Arquivo {remove} removido com sucesso\n")
+    else:
+        return sys.stdout.write("Não há elementos\n")
 
 
 def file_metadata(instance, position):
