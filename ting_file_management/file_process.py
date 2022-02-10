883a057c55539860ab23d10d985b04a5e2451a5c
Mathaus Annibal <mathausannibal@gmail.com>
2022-02-09 21:10:43 -0300

    adiciona requisito 3

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,18 @@
+from ting_file_management.file_management import txt_importer
+import sys
+
+
 def process(path_file, instance):
     """Aqui irá sua implementação"""
+    if instance.__len__() < 1:
+        arq = txt_importer(path_file)
+        data = {
+            "nome_do_arquivo": path_file,
+            "qtd_linhas": len(arq),
+            "linhas_do_arquivo": arq,
+        }
+        instance.enqueue(data)
+        return sys.stdout.write(f"{data}")
 
 
 def remove(instance):
