8cbc3146d362f67fdbca8091c4cb7c2e1ac725b7
Parker9309 <juan.tentiomaru9309@gmail.com>
2022-02-10 10:27:59 -0300

    feat: req 6 test

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,18 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    list()
+    for gokuSsj in range(instance.__len__()):
+        search = instance.list(gokuSsj)
+        data = {
+            "palavra": word,
+            "arquivo": search["nome_do_arquivo"],
+            "ocorrencias": [],
+        }
+        for count, line in enumerate(search["linhas_do_arquivo"]):
+            if word.upper() in line.upper():
+                data["ocorrencias"].append({"linha": count + 1})
+        if len(data["ocorrencias"]) > 0:
+            list.append(data)
+    return list
 
 
 def search_by_word(word, instance):
