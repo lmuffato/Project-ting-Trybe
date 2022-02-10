19c7ea2ccf05e5dbf0e6c563abcaf2ebe7f32c15
VRodriguera <V.rodriguesjk@gmail.com>
2022-02-10 20:51:42 -0300

    feat: :sparkles: Req 3

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,19 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    for i in range(len(instance)):
+        if instance.search(i)['nome_do_arquivo'] == path_file:
+            return None
+    import_file = txt_importer(path_file)
+    processed_file = {
+        'nome_do_arquivo': path_file,
+        'qtd_linhas': len(import_file),
+        'linhas_do_arquivo': import_file
+    }
+    instance.enqueue(processed_file)
+    sys.stdout.write(str(processed_file))
 
 
 def remove(instance):
