3dbd5e1205fadeb68743e287cffffc29b0c41f2b
Rmgreco <rafaelmgreco@hotmail.com>
2022-02-10 16:26:55 -0300

    6

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,21 @@
 def exists_word(word, instance):
     """começando"""
+    result = list()
+    for element in instance._data:
+        object = {
+            "palavra": word,
+            "arquivo": element["nome_do_arquivo"],
+            "ocorrencias": list(),
+        }
+    for index, line in enumerate(element["linhas_do_arquivo"]):
+        if word in line:
+            res = {"linha": index + 1}
+            object["ocorrencias"].append(res)
+        else:
+            return []
+        result.append(object)
+
+    return result
 
 
 def search_by_word(word, instance):
