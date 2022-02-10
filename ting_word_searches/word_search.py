3ad833a877451a67cd8800f0183244c9a0b100ac
rafael <rafittu@outlook.com>
2022-02-10 15:57:51 -0300

    feat req 6

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,21 @@
+from ting_file_management.file_management import txt_importer
+
+
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    occurrences = []
+    words_found_list = []
+
+    for file in instance.queue:
+        txt_file = txt_importer(file)
+        index = 1
+        while len(txt_file) >= index:
+            if word.lower() in txt_file[index - 1].lower():
+                occurrences.append({"linha": index})
+            index += 1
+        words_found_list.append(
+            {"palavra": word, "arquivo": file, "ocorrencias": occurrences}
+        )
+    return [] if len(occurrences) == 0 else words_found_list
 
 
 def search_by_word(word, instance):
