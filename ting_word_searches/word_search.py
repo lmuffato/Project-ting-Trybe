92b62197e86ec6b38e5c09c108d7ab37b48f7655
biazidioti <bzferreira.bio@gmail.com>
2022-02-10 16:39:49 -0300

    Passou? Eh tetraaaaaa? Vem ni mmim emprego

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,23 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    data = []
+
+    for item in instance.list:
+        acc = []
+        data.append({
+            'palavra': word,
+            'arquivo': item['nome_do_arquivo'],
+            'ocorrencias': acc
+        })
+
+        for index, line in enumerate(item['linhas_do_arquivo']):
+            if word in line:
+                acc.append({
+                    'linha': index + 1
+                })
+        if not len(acc):
+            data.pop()
+
+    return data
 
 
 def search_by_word(word, instance):
