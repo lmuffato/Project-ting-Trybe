f8f309ea193c8149abbd1a64781d3deb4f9b1608
moraesvictor <moraesvictor@gmail.com>
2022-02-10 19:25:23 -0300

    feat: 1

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -1,15 +1,22 @@
 class Queue:
     def __init__(self):
         """Inicialize sua estrutura aqui"""
+        self.queue = []
 
     def __len__(self):
         """Aqui irá sua implementação"""
+        return len(self.queue)
 
     def enqueue(self, value):
         """Aqui irá sua implementação"""
+        self.queue.append(value)
 
     def dequeue(self):
         """Aqui irá sua implementação"""
+        return self.queue.pop(0)
 
     def search(self, index):
         """Aqui irá sua implementação"""
+        if not (0 <= index <= len(self.queue)):
+            raise IndexError()
+        return self.queue[index]
