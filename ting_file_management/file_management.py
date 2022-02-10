535364e999320f7ccea755cea5a0cf7633d47c6b
Felipe Shcolnik <shcolnik@gmail.com.br>
2022-02-10 17:49:34 -0300

    fct text_importer

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,8 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if not path_file[-3:] == 'txt':
+        return sys.stderr.write('Formato inválido\n')
+    with open(path_file, 'r') as file:
+        return file.read().splitlines()
