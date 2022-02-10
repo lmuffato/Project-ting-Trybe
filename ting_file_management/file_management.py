5efb7d759a23ecd9e80f1eb8c7a91dca4e722d1e
Orlando Flores <orlando_flores@id.uff.br>
2022-02-10 17:59:14 -0300

    feat: requisito 2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,13 @@
+import sys
+
+accepted_file_type = ".txt"
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if accepted_file_type not in path_file:
+        return sys.stderr.write("Formato inválido\n")
+    try:
+        return open(path_file, mode="r").read().splitlines()
+
+    except FileNotFoundError:
+        return sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
