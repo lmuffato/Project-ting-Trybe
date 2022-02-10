600a70aa27b5a54ca5a77e981b2b358dccac5afa
VRodriguera <V.rodriguesjk@gmail.com>
2022-02-10 20:41:55 -0300

    feat: :sparkles: Req 2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,14 @@
+import sys
+from os.path import exists
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if (path_file[-3:] != 'txt'):
+        print('Formato inválido', file=sys.stderr)
+
+    if not exists(path_file):
+        return print(f'Arquivo {path_file} não encontrado', file=sys.stderr)
+
+    with open(path_file, mode='r') as file:
+        file_row = file.read().splitlines()
+        return file_row
