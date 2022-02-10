89efd9887cf6c585d59887befed9013efed239a5
Lucas Machado <lucas.almeida.machado92@gmail.com>
2022-02-09 23:35:48 -0300

    Req 3.2 - retornando mensagem via stdout - adicao do conteudo na fila

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,19 @@
+from ting_file_management.file_management import txt_importer
+import sys
+
+
 def process(path_file, instance):
     """Aqui irá sua implementação"""
+    info_file = txt_importer(path_file)
+
+    info_return = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(info_file),
+        "linhas_do_arquivo": info_file
+    }
+
+    instance.enqueue(info_return)
+    print(info_return, file=sys.stdout)
 
 
 def remove(instance):
