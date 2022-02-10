32cc5c017adf3bd7d39ab869c154858a0849abc4
Marcelofcdantas <65692996+Marcelofcd@users.noreply.github.com>
2022-02-10 19:02:33 -0300

    fix: requisito 7

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,27 @@
+import re
+
+
+def searching_and_counting_word(data, word, index):
+    quantity = []
+    list = []
+    for line in data["linhas_do_arquivo"]:
+        if re.search(word, line, re.IGNORECASE):
+            quantity.append({"linha": index + 1})
+            list.append(
+                {
+                    "palavra": f"{word}",
+                    "arquivo": data["nome_do_arquivo"],
+                    "ocorrencias": quantity,
+                }
+            )
+    return list
+
+
 def exists_word(word, instance):
-    return []
+    for index in range(len(instance)):
+        data = instance.search(index)
+        result = searching_and_counting_word(data, word, index)
+    return result
 
 
 def search_by_word(word, instance):
