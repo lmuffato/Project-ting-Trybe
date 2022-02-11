1828262a6cf635a048fc21185dfff50a848b7078
jvitorof <j.vitoroflores@gmail.com>
2022-02-10 21:30:44 -0300

    🔴🔥🌮🌶 Guess Who?

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,12 @@
+import sys
+
+
 def txt_importer(path_file):
     """Aqui irá sua implementação"""
+    if not path_file.endswith(".txt"):
+        return sys.stderr.write("Formato inválido\n")
+    try:
+        with open(path_file, "r") as file:
+            return file.read().splitlines()
+    except FileNotFoundError:
+        return sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
