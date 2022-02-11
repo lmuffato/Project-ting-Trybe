3701395e84dd7a93e93f9f626e621ea81b5898f8
jvitorof <j.vitoroflores@gmail.com>
2022-02-10 22:03:53 -0300

    JAZZ SABBATH 🎷🥁🎹🎸

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -4,6 +4,10 @@ from ting_file_management.file_management import txt_importer
 
 def process(path_file, instance):
     """Aqui irá sua implementação"""
+    for file in range(len(instance)):
+        if instance.search(file)["nome_do_arquivo"] == path_file:
+            return None
+
     doc = txt_importer(path_file)
 
     doc_data = {
@@ -19,7 +23,19 @@ def process(path_file, instance):
 
 def remove(instance):
     """Aqui irá sua implementação"""
+    try:
+        file = instance.dequeue()
+        file_name = file["nome_do_arquivo"]
+        sys.stdout.write(f"Arquivo {file_name} removido com sucesso\n")
+
+    except Exception:
+        sys.stdout.write("Não há elementos\n")
 
 
 def file_metadata(instance, position):
     """Aqui irá sua implementação"""
+    try:
+        file = instance.search(position)
+        sys.stdout.write(str(file))
+    except IndexError:
+        sys.stderr.write("Posição inválida\n")
