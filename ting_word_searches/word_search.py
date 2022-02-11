e9a5d118febcf7e7f6b99caa2f059a5ce1c3a378
Lucas Machado <lucas.almeida.machado92@gmail.com>
2022-02-10 22:48:39 -0300

    Req 7: escreve a frase que contem a palavra buscada

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -21,3 +21,26 @@ def exists_word(word, instance):
 
 def search_by_word(word, instance):
     """Aqui irá sua implementação"""
+    info_file = []
+    count = 0
+    # primeiro, percorro os elementos da pilha
+    for index in instance._data:
+        info_return = {
+            "palavra": word,
+            "arquivo": index["nome_do_arquivo"],
+            "ocorrencias": []
+        }
+    # segundo, percorro o texto
+        for line in index["linhas_do_arquivo"]:
+            # coloquei o upper() pq a palavra buscada esta
+            # em minusculo e a palavra do conteudo esta com
+            # a inicial maiuscula
+            if word.upper() in line.upper():
+                count += 1
+                info_return["ocorrencias"].append({
+                    "linha": count,
+                    "conteudo": line,
+                })
+        if len(info_return["ocorrencias"]) > 0:
+            info_file.append(info_return)
+    return info_file
