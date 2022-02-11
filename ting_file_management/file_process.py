526dd1e1808c77dc75190a3a3fd061788eab57b6
negres <nataliaknegres@gmail.com>
2022-02-11 11:48:10 -0300

    feat: req 3

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,22 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    if instance.__len__() == 0:
+        file = txt_importer(path_file)
+
+        file_name = f"'nome_do_arquivo': '{path_file}'"
+        lines_counter = f"'qtd_linhas': {len(file)}"
+        lines = f"'linhas_do_arquivo': {file}"
+
+        sys.stdout.write(f'{file_name}')
+        sys.stdout.write(f'{lines_counter}')
+        sys.stdout.write(f'{lines}')
+
+        instance.enqueue(path_file)
+    else:
+        pass
 
 
 def remove(instance):
