4d0b533049f7b3ffc7e09c1717182bc451afe279
Anderson F. S. do Nascimento <anderson.nascimento@meta.com.br>
2022-02-11 00:42:04 -0300

    add requisito 6

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,23 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    word_exists = [
+        {
+            "palavra": word,
+            "arquivo": txt_file["nome_do_arquivo"],
+            "ocorrencias": [],
+        }
+        for txt_file in instance.queueFifo
+    ]
+
+    for txt_file in instance.queueFifo:
+        for index, line in enumerate(txt_file["linhas_do_arquivo"]):
+            current_line = index + 1
+            if word.lower() in line.lower():
+                word_exists[index]["ocorrencias"].append(
+                    {"linha": current_line}
+                )
+            if len(word_exists[index]["ocorrencias"]) > 0:
+                return word_exists
+        return []
 
 
 def search_by_word(word, instance):
