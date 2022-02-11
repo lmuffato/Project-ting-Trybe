f8a3b2621bfe28648000c0a972f03f8df91f3138
Wanderson <wandersonsales.dev@gmail.com>
2022-02-11 02:24:51 -0300

    :sparkles: Requirement 06
    6 - Implemente uma função exists_word dentro do módulo word_search, que valide a existência da palavra em todos os arquivos processados. Para cada palavra encontrada, deve-se listar sua linha conforme apresentação abaixo.

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,25 @@
+# Para o desenvolvimento desta função, estudei um pouco do código
+# do Vinicius Rodrigues presente na PR abaixo:
+# https://github.com/tryber/sd-010-a-project-ting/pull/78
+
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    for row in range(len(instance)):
+        details = []
+        row_number = 0
+        search = instance.search(row)
+        data = {
+            "palavra": word,
+            "arquivo": search['nome_do_arquivo'],
+            "ocorrencias": []
+        }
+
+        for match in search['linhas_do_arquivo']:
+            if word.lower() in match.lower():
+                row_number += 1
+                data["ocorrencias"].append({"linha": row_number})
+        if len(data["ocorrencias"]) > 0:
+            details.append(data)
+    return details
 
 
 def search_by_word(word, instance):
