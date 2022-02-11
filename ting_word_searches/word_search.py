96e29c68f55d04c010f8b6d3fc28c14a8b129409
Felipe Shcolnik <shcolnik@gmail.com.br>
2022-02-11 11:00:10 -0300

    exist_word fct

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,21 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    palavra = []
+    content = instance.fila[0]
+    print(content)
+    for line in content["linhas_do_arquivo"]:
+        if word in line:
+            palavra.append(
+                {"linha": content["linhas_do_arquivo"].index(line) + 1}
+            )
+    result = [{
+        "palavra": word,
+        "arquivo": content["nome_do_arquivo"],
+        "ocorrencias": palavra,
+    }]
+    if result == []:
+        return None
+    print(result)
+    return result
 
 
 def search_by_word(word, instance):
