a862e8abba6cbde9eaf0fdaf82ea43d6346aecca
Anderson F. S. do Nascimento <anderson.nascimento@meta.com.br>
2022-02-11 00:06:41 -0300

    add requisito 3

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,10 +1,36 @@
+import sys
+from ting_file_management import file_management
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    dataFileTxt = file_management.txt_importer(path_file)
+
+    data = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(dataFileTxt),
+        "linhas_do_arquivo": dataFileTxt,
+    }
+
+    for item in range(0, instance.__len__()):
+        if path_file == instance.search(item)["nome_do_arquivo"]:
+            return None
+
+    sys.stdout.write(str(data))
+    instance.enqueue(data)
 
 
 def remove(instance):
-    """Aqui irá sua implementação"""
+    if instance.__len__() <= 0:
+        sys.stdout.write("Não há elementos\n")
+        return
+
+    elementRemoved = instance.dequeue()["nome_do_arquivo"]
+    sys.stdout.write(f"Arquivo {elementRemoved} removido com sucesso\n")
 
 
 def file_metadata(instance, position):
-    """Aqui irá sua implementação"""
+    try:
+        data = instance.search(position)
+        sys.stdout.write(str(data))
+    except IndexError:
+        sys.stderr.write("Posição inválida\n")
