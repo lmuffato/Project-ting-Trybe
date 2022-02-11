daba718fab11a149915226816bf4e2f8969e1527
negres <nataliaknegres@gmail.com>
2022-02-11 09:50:53 -0300

    feat: req 2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,12 @@
+import sys
+import os.path
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if not path_file.endswith('.txt'):
+        return sys.stderr.write('Formato inválido\n')
+    if not os.path.isfile(path_file):
+        return sys.stderr.write(f'Arquivo {path_file} não encontrado\n')
+
+    file_list = open(path_file, 'r').read().splitlines()
+    return file_list
