77eab7a55919323845e59e9890f236c9ff0fffaf
jvitorof <j.vitoroflores@gmail.com>
2022-02-10 21:46:55 -0300

    🔴🔥🌮🌶 Guess Who?

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,21 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
     """Aqui irá sua implementação"""
+    doc = txt_importer(path_file)
+
+    doc_data = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(doc),
+        "linhas_do_arquivo": doc,
+    }
+
+    instance.enqueue(doc_data)
+
+    return sys.stdout.write(str(doc_data))
+
 
 
 def remove(instance):
