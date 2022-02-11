d6f443587a83f1718c4f5eff5efcae5dc56763d1
jvitorof <j.vitoroflores@gmail.com>
2022-02-11 08:16:54 -0300

    🎼Shadow Dancing🥁

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,20 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    lineIndex = 0
+    response = list()
+    for file in range(len(instance)):
+        searchResult = {
+            "palavra": word,
+            "arquivo": file['nome_do_arquivo'],
+            "ocorrencias": []
+        }
+        for phrase in file["linhas_do_arquivo"]:
+            if word.lower() in phrase.lower():
+                lineIndex += 1
+                searchResult["ocorrencias"].append({"linha": lineIndex})
+        if len(searchResult["ocorrencias"]) > 0:
+            response.append(searchResult)
+
+    return response
 
 
 def search_by_word(word, instance):
