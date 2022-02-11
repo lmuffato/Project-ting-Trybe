f85d2b920cf7c6890eecc940fb51d058ad654017
Mathaus Annibal <mathausannibal@gmail.com>
2022-02-10 23:06:01 -0300

    adiciona requito 6

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,19 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    for index in range(instance.__len__()):
+        file_data = instance.search(index)
+        data = {
+            'palavra': word,
+            'arquivo': file_data['nome_do_arquivo'],
+            'ocorrencias': []
+        }
+        index = 0
+        for line in file_data['linhas_do_arquivo']:
+            index += 1
+            if word in line:
+                data['ocorrencias'].append({'linha': index})
+        if (len(data['ocorrencias']) <= 0):
+            return []
+        return [data]
 
 
 def search_by_word(word, instance):
