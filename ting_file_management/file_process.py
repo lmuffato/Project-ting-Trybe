6c32941b3b0b5c32947ea24112b2d5e8a9a7df1a
Wanderson <wandersonsales.dev@gmail.com>
2022-02-11 01:44:54 -0300

    :sparkles: Requirement 03
    3 - Implemente uma função process dentro do módulo file_process capaz de ler o arquivo carregado na função anterior e efetuar o preprocessamento do conteúdo.

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,16 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    data = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(txt_importer(path_file)),
+        "linhas_do_arquivo": txt_importer(path_file)
+    }
+    if data not in instance._data:
+        instance.enqueue(data)
+    sys.stdout.write(str(data))
 
 
 def remove(instance):
