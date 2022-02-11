baf6afbd4401465fe3a5d0b7f748a84d5580711b
Wanderson <wandersonsales.dev@gmail.com>
2022-02-11 01:33:11 -0300

    :sparkles: Requirement 01
    1 - Implemente uma fila para armazenar os arquivos que serão lidos.

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -1,16 +1,20 @@
-# Iniciando o projeto
 class Queue:
     def __init__(self):
-        """Inicialize sua estrutura aqui"""
+        self._data = list()
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        return len(self._data)
+
+    def is_empty(self):
+        return not bool(self.size())
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        self._data.append(value)
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        return self._data.pop(0)
 
     def search(self, index):
-        """Aqui irá sua implementação"""
+        if index >= 0 and self._data[index]:
+            return self._data[index]
+        raise IndexError
