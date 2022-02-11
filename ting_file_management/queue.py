0a4ec87bd259019b1903919e5a0cb79b1aa9522b
negres <nataliaknegres@gmail.com>
2022-02-11 09:37:25 -0300

    feat: req 1

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -1,15 +1,18 @@
 class Queue:
     def __init__(self):
-        """Inicialize sua estrutura aqui"""
+        self.data = list()
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        return len(self.data)
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        self.data.append(value)
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        return self.data.pop(0)
 
     def search(self, index):
-        """Aqui irá sua implementação"""
+        if index in range(len(self.data)):
+            return self.data[index]
+        else:
+            raise IndexError
