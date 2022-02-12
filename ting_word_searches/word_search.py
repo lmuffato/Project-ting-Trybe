42c5099ab389e6e67813db511eef3ccf8e663b01
jvitorof <j.vitoroflores@gmail.com>
2022-02-12 16:24:28 -0300

    🫁📣ACABOU com Chave de Diamante! SHINE - COLETIVE SOUL 😭 Tchau Querida! Obrigado Trybe, Obrigado Instruçao e Facilitaçao. Valeu T7 🔪🧀💜 Valeu T6, T8, T9 e T10

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,21 +1,52 @@
+from ting_file_management.file_process import process
+from ting_file_management.queue import Queue
+
+
 def exists_word(word, instance):
-    lineIndex = 0
+    """Aqui irá sua implementação"""
     response = list()
+    lineIndex = 0
+
     for file in range(len(instance)):
+
+        search = instance.search(file)
+
         searchResult = {
             "palavra": word,
-            "arquivo": file['nome_do_arquivo'],
+            "arquivo": search["nome_do_arquivo"],
             "ocorrencias": []
         }
-        for phrase in file["linhas_do_arquivo"]:
-            if word.lower() in phrase.lower():
+
+        for line in range(len(search["linhas_do_arquivo"])):
+            if word.lower() in search["linhas_do_arquivo"][line].lower():
                 lineIndex += 1
                 searchResult["ocorrencias"].append({"linha": lineIndex})
-        if len(searchResult["ocorrencias"]) > 0:
-            response.append(searchResult)
+            if len(searchResult["ocorrencias"]) > 0:
+                response.append(searchResult)
 
     return response
 
 
 def search_by_word(word, instance):
     """Aqui irá sua implementação"""
+    response = list()
+    lineIndex = 0
+
+    for file in range(len(instance)):
+
+        search = instance.search(file)
+
+        searchResult = {
+            "palavra": word,
+            "arquivo": search["nome_do_arquivo"],
+            "ocorrencias": []
+        }
+
+        for line in range(len(search["linhas_do_arquivo"])):
+            if word.lower() in search["linhas_do_arquivo"][line].lower():
+                lineIndex += 1
+                searchResult["ocorrencias"].append({"linha": lineIndex, "conteudo": search["linhas_do_arquivo"][line]})
+            if len(searchResult["ocorrencias"]) > 0:
+                response.append(searchResult)
+
+    return response
