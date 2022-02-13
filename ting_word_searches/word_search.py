7364be65d6b2c9134b0452122a4be653131a5347
lucas-paz <lucas.hpaz@outlook.com>
2022-02-13 10:38:37 -0300

    Req 7

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -20,4 +20,23 @@ def exists_word(word, instance):
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    for i in range(len(instance)):
+        search_file = instance.search(i)
+
+        params = {
+            "palavra": word,
+            "arquivo": search_file["nome_do_arquivo"],
+            "ocorrencias": []}
+
+        search_details = []
+        row_number = 0
+
+        for i in search_file["linhas_do_arquivo"]:
+            if word.lower() in i.lower():
+                row_number += 1
+                params["ocorrencias"].append(
+                    {"linha": row_number, "conteudo": i}
+                )
+        if len(params["ocorrencias"]) > 0:
+            search_details.append(params)
+    return search_details
