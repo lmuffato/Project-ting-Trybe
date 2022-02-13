3a55bd0e64ae515b4a402afa7e80eea1b81d0c47
lucas-paz <lucas.hpaz@outlook.com>
2022-02-13 10:19:26 -0300

    Req 2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,12 @@
+import sys
+from os.path import exists
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    array_name = path_file.split(",")
+    if (array_name[len(array_name) - 1] != 'txt'):
+        print('Formato inválido', file=sys.stderr)
+    if not exists(path_file):
+        return print(f'Arquivo {path_file} não encontrado', file=sys.stderr)
+
+    return open(path_file, mode="r").read().splitlines()
