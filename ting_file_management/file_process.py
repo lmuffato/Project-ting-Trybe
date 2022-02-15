a339c9886bae5a415f67e42a16803641e7bc6985
vinicios <soicinivt@gmail.com>
2022-02-15 15:24:28 -0300

    feat(req03): function process in file_process.py

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,25 @@
+import sys
+
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    if not len(instance):  # if length == 0
+        lines = txt_importer(path_file)
+        instance.enqueue(lines)
+        # source:
+        # https://www.geeksforgeeks.org/how-to-print-to-stderr-and-stdout-in-python/
+        print(
+            {
+                "nome_do_arquivo": f"{path_file}",
+                "qtd_linhas": len(lines),
+                "linhas_do_arquivo": lines,
+            },
+            file=sys.stdout,
+        )
+
+    else:
+        return instance
 
 
 def remove(instance):
