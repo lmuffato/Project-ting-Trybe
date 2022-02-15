e6740c6b843aeb8fb44b36cdb425c6be6ee11abd
vinicios <soicinivt@gmail.com>
2022-02-15 16:05:34 -0300

    fix(req03): add correct content in queue

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -6,15 +6,16 @@ from ting_file_management.file_management import txt_importer
 def process(path_file, instance):
     if not len(instance):  # if length == 0
         lines = txt_importer(path_file)
-        instance.enqueue(lines)
+        processed_content = {
+            "nome_do_arquivo": f"{path_file}",
+            "qtd_linhas": len(lines),
+            "linhas_do_arquivo": lines,
+        }
+        instance.enqueue(processed_content)
         # source:
         # https://www.geeksforgeeks.org/how-to-print-to-stderr-and-stdout-in-python/
         print(
-            {
-                "nome_do_arquivo": f"{path_file}",
-                "qtd_linhas": len(lines),
-                "linhas_do_arquivo": lines,
-            },
+            processed_content,
             file=sys.stdout,
         )
 
@@ -23,7 +24,10 @@ def process(path_file, instance):
 
 
 def remove(instance):
-    """Aqui irá sua implementação"""
+    if not len(instance):
+        print("Não há elementos", file=sys.stdout)
+    else:
+        pass
 
 
 def file_metadata(instance, position):
