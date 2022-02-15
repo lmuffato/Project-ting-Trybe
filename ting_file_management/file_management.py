cb7fe6e2e61a184d8f5285a16304b81f9ef4b520
vinicios <soicinivt@gmail.com>
2022-02-15 12:53:40 -0300

    feat(req02): function txt_importer in file_management.py

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,31 @@
+import pathlib
+import sys
+
+
+def inport_news(path_file):
+    try:
+        with open(path_file, "r") as file:
+            data = file.read()
+            file_lines = data.split("\n")
+            return file_lines
+
+    except FileNotFoundError:
+        sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
+
+
 def txt_importer(path_file):
-    """Hello dear"""
+    if path_file:
+        if_correct_path = {
+            ".txt": inport_news(path_file),
+        }
+
+        try:
+            extension = pathlib.Path(path_file).suffix
+            call_function = if_correct_path[extension]
+            return call_function
+
+        except KeyError:
+            sys.stderr.write("Formato inválido\n")
+
+    else:
+        print(f"Arquivo não encontrado")
