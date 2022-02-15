5da8befb1e5ba3ae81d7cdb0173b1be01aa34d3d
vinicios <soicinivt@gmail.com>
2022-02-15 10:28:21 -0300

    feat(req01.2): method dequeue in queue.py

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -9,7 +9,7 @@ class Queue:
         self.queue.append(value)
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        return self.queue.pop(0)
 
     def search(self, index):
         """Aqui irá sua implementação"""
