fee5b9a4c7fcf27ec8acd63f08a35fee309c8b60
vinicios <soicinivt@gmail.com>
2022-02-15 18:26:02 -0300

    feat(req06): function exists_word in word_search.py

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,25 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    word_occurrencies = list()
+    length = instance.__len__()
+    if not length:
+        return word_occurrencies
+    else:
+        occurrencies = list()
+        content = instance.search(0)
+        for index, words in enumerate(content["linhas_do_arquivo"]):
+            if word.lower() in words.lower():
+                occurrencies.append({"linha": index + 1})
+        if occurrencies:
+            word_occurrencies.append(
+                {
+                    "palavra": word,
+                    "arquivo": instance.search(0)["nome_do_arquivo"],
+                    "ocorrencias": occurrencies,
+                }
+            )
+            return word_occurrencies
+        else:
+            return word_occurrencies
 
 
 def search_by_word(word, instance):
