bfc9bc4abef1eb8a38257c400cdac77841e5e086
Edrei <edreimateus@gmail.com>
2022-02-16 20:08:27 -0300

    feat 3 requiriment

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,21 @@
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
     """Aqui irá sua implementação"""
+    for index in range(len(instance)):
+        if path_file == instance.search(index)["nome_do_arquivo"]:
+            return None
+
+    file = txt_importer(path_file)
+    data = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(file),
+        "linhas_do_arquivo": file,
+    }
+
+    instance.enqueue(data)
+    print(data)
 
 
 def remove(instance):
