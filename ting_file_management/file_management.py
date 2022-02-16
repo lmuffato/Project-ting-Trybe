1a29d25860d49c1bbe28fbfd81e9f57cdbb5a3b9
luizfelipe-dev <thelastnailmusic@gmail.com>
2022-02-16 20:31:01 -0300

    desafio 2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,14 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    try:
+        if not path_file.endswith('.txt'):
+            sys.stderr.write("Formato inválido\n")
+            return None
+
+        with open(path_file, mode='r') as file:
+            return file.read().split('\n')
+
+    except FileNotFoundError:
+        sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
