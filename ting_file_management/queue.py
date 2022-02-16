c3e40a0f838d97a8dcf6fea2e9c17f7f5a68eb74
Edrei <edreimateus@gmail.com>
2022-02-16 00:47:26 -0300

    feat 1 requiriment

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -1,15 +1,22 @@
 class Queue:
     def __init__(self):
         """Inicialize sua estrutura aqui"""
+        self.data = list()
 
     def __len__(self):
         """Aqui irá sua implementação"""
+        return len(self.data)
 
     def enqueue(self, value):
         """Aqui irá sua implementação"""
+        self.data.append(value)
 
     def dequeue(self):
         """Aqui irá sua implementação"""
+        return self.data.pop(0)
 
     def search(self, index):
         """Aqui irá sua implementação"""
+        if index < 0 or index > (self.__len__() - 1):
+            raise IndexError
+        return self.data[index]
