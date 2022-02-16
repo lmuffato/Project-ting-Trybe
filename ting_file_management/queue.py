8a75be16b2a7d009c03e4f828f24629dc17398b3
luizfelipe-dev <thelastnailmusic@gmail.com>
2022-02-16 20:08:25 -0300

    desafio 1

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -1,16 +1,19 @@
 class Queue:
     def __init__(self):
-        """Inicialize sua estrutura aqui"""
-        print('start')
+        self.data = []
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        return len(self.data)
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        self.data.append(value)
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        return self.data.pop(0)
 
     def search(self, index):
-        """Aqui irá sua implementação"""
+        if 0 <= index <= self.__len__():
+            return self.data[index]
+
+        else:
+            raise IndexError
