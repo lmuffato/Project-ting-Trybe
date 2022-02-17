725f1d28a1dd9535e848ab1ac51e0dfc098f0cd0
luizfelipe-dev <thelastnailmusic@gmail.com>
2022-02-17 18:38:56 -0300

    desafios 3 e 4

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,9 +1,33 @@
+from ting_file_management.file_management import txt_importer
+# import sys
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    data = txt_importer(path_file)
+
+    file_to_compare = {
+        'nome_do_arquivo': path_file,
+        'qtd_linhas': len(data),
+        'linhas_do_arquivo': data
+    }
+
+    for text in instance.data:
+        if text == file_to_compare:
+            return None
+
+    instance.enqueue(file_to_compare)
+
+    print(file_to_compare)
 
 
 def remove(instance):
-    """Aqui irá sua implementação"""
+    if not instance.__len__():
+        print('Não há elementos')
+        return None
+
+    file = instance.dequeue()
+
+    print(f"Arquivo {file['nome_do_arquivo']} removido com sucesso")
 
 
 def file_metadata(instance, position):
