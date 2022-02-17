391799358d646703ca27daa42954a2ae3483f035
Edrei <edreimateus@gmail.com>
2022-02-16 21:13:06 -0300

    feat: 6 requiriment

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,29 @@
 def exists_word(word, instance):
     """Aqui irá sua implementação"""
+    data = []
+
+    for index in range(len(instance)):
+        file = instance.search(index)
+
+        word_ocorruncy = {
+          "palavra": word,
+          "arquivo": file["nome_do_arquivo"],
+          "ocorrencias": [],
+        }
+
+        for item in file["linhas_do_arquivo"]:
+            line = 0
+
+            if word in item:
+                line += 1
+                word_ocorruncy["ocorrencias"].append({"linha": line})
+
+        if len(word_ocorruncy["ocorrencias"]) == 0:
+            return []
+
+        data.append(word_ocorruncy)
+
+    return data
 
 
 def search_by_word(word, instance):
