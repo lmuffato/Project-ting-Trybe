733b6cb9e9a843b294f2f11375f49a848410ebc8
ScriptCamilo <script.camilo@gmail.com>
2022-02-19 18:44:44 -0300

    First requirement done

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -1,15 +1,19 @@
 class Queue:
     def __init__(self):
-        """Inicialize sua estrutura aqui"""
+        self.data = list()
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        return len(self.data)
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        self.data.append(value)
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        value = self.data[0]
+        del self.data[0]
+        return value
 
     def search(self, index):
-        """Aqui irá sua implementação"""
+        if (index < 0):
+            raise(IndexError)
+        return self.data[index]
