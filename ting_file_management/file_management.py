61a81dc8f40a60cfe527438af0bc41874fe25a61
ScriptCamilo <script.camilo@gmail.com>
2022-02-19 19:30:53 -0300

    Second requirement done

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,15 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    ext = path_file.split('.')[1]
+    if (ext != 'txt'):
+        return print('Formato inválido', file=sys.stderr)
+    try:
+        with open(path_file) as file:
+            lines = list()
+            for line in file.readlines():
+                lines.append(line.replace('\n', ''))
+            return lines
+    except FileNotFoundError:
+        print(f'Arquivo {path_file} não encontrado', file=sys.stderr)
