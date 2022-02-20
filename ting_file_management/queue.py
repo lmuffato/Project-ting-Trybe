10640247d03c859fdf3fb6ae9be7b506ad7f868c
lmuffato <lmiremuffato@gmail.com>
2022-02-20 20:20:08 -0300

    requisito 1

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -1,15 +1,33 @@
 class Queue:
+    # CONSTRUTOR
     def __init__(self):
-        """Inicialize sua estrutura aqui"""
+        # list() -> usar estruturta de lista para guardar
+        # as informações da pilha
+        self.list = list()
 
+    # RETORNA O COMPRIMENTO DA LISTA
     def __len__(self):
-        """Aqui irá sua implementação"""
+        # método para verifica o comprimento da lista
+        return len(self.list)
 
+    # ADICIONAR DADOS
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        # adiciona o dado no topo da lista
+        return self.list.append(value)
 
+    # REMOVER DADOSS
     def dequeue(self):
-        """Aqui irá sua implementação"""
-
+        # remove o dado no topo da lista
+        return self.list.pop(0)
+        
+    # BUSCAR DADO    
     def search(self, index):
-        """Aqui irá sua implementação"""
+        # Se o index for menor que 0 ou maior que a lista
+        if(index < 0 or index > len(self.list)):
+            # retorna a exception erro indexError
+            raise IndexError
+        return self.list[index]
+
+
+# teste automatizado
+# python3 -m pytest tests/test_queue.py
