f84cd6f4d472ca87efa845224ee9a3978e8eec08
Micaela Alves <micaela.alves93@gmail.com>
2022-02-20 20:14:36 -0300

    feat - requisito 4

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -17,7 +17,12 @@ def process(path_file, instance):
 
 
 def remove(instance):
-    """Aqui irá sua implementação"""
+    if not instance:
+        sys.stdout.write("Não há elementos\n")
+        return None
+    else:
+        path_file = instance.dequeue()["nome_do_arquivo"]
+        sys.stdout.write(f"Arquivo {path_file} removido com sucesso\n")
 
 
 def file_metadata(instance, position):
