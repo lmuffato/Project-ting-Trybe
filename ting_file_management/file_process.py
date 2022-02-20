9a2f40c330f87e51d8e2e6bb502fbe986a468ba5
ScriptCamilo <script.camilo@gmail.com>
2022-02-19 21:28:30 -0300

    Third requirement done

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,23 @@
+from ting_file_management.file_management import txt_importer
+import sys
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    size = len(instance)
+    lines = txt_importer(path_file)
+    infos = {
+        'nome_do_arquivo': path_file,
+        'qtd_linhas': len(lines),
+        'linhas_do_arquivo': lines,
+    }
+
+    for n in range(size):
+        info = instance.search(n)
+        if info['nome_do_arquivo'] == path_file:
+            return
+
+    instance.enqueue(infos)
+    return print(infos, file=sys.stdout)
 
 
 def remove(instance):
