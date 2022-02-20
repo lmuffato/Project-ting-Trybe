0904986484362b357ea90b98f142a2b1238263d9
Micaela Alves <micaela.alves93@gmail.com>
2022-02-20 20:35:28 -0300

    feat - requisito 5

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -26,4 +26,7 @@ def remove(instance):
 
 
 def file_metadata(instance, position):
-    """Aqui irá sua implementação"""
+    try:
+        return sys.stdout.write(str(instance.search(position)))
+    except IndexError:
+        return sys.stderr.write("Posição inválida")
