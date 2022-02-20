95edf93dfcd76079ee960c6378b48caa677e378a
Micaela Alves <micaela.alves93@gmail.com>
2022-02-20 20:05:48 -0300

    feat - requesito 3

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,19 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    for data in instance.data:
+        if data["nome_do_arquivo"] == path_file:
+            return None
+    data = txt_importer(path_file)
+    content = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(data),
+        "linhas_do_arquivo": data,
+    }
+    instance.enqueue(content)
+    sys.stdout.write(str(content))
 
 
 def remove(instance):
