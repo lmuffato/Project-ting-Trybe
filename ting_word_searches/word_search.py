d8e6752eec9ee46e1fc56d0a1b5563c86f8beec1
ScriptCamilo <script.camilo@gmail.com>
2022-02-20 09:10:02 -0300

    Seventh requirement done

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -13,7 +13,7 @@ def exists_word(word, instance):
 
         for n in range(qtd_linhas):
             line = lines[n]
-            if word in line:
+            if word.lower() in line.lower():
                 msg['ocorrencias'].append({
                     'linha': n + 1,
                 })
@@ -24,4 +24,26 @@ def exists_word(word, instance):
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    searchs = []
+    size = len(instance)
+
+    for n in range(size):
+        info = instance.search(n)
+        file, qtd_linhas, lines = info.values()
+        msg = {
+            'palavra': word,
+            'arquivo': file,
+            'ocorrencias': []
+        }
+
+        for n in range(qtd_linhas):
+            line = lines[n]
+            if word.lower() in line.lower():
+                msg['ocorrencias'].append({
+                    'linha': n + 1,
+                    'conteudo': line,
+                })
+        if len(msg['ocorrencias']) > 0:
+            searchs.append(msg)
+
+    return searchs
