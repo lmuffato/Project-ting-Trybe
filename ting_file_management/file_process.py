d833ca375978e449ef140cfcf19e05aa563f66cc
rodolfo_oliveira <rod.vgo@gmail.com>
2022-02-20 10:10:02 -0300

    RODOLFO TING - Acabou

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,10 +1,33 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    content = txt_importer(path_file)
+    file_data = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(content),
+        "linhas_do_arquivo": content,
+    }
+
+    if len(instance) <= 0 or instance.search(0) != file_data:
+        instance.enqueue(file_data)
+        print(file_data)
 
 
 def remove(instance):
-    """Aqui irá sua implementação"""
+    if len(instance) == 0:
+        return print("Não há elementos")
+
+    deleted_file = instance.dequeue()
+    file_name = deleted_file["nome_do_arquivo"]
+
+    print(f"Arquivo {file_name} removido com sucesso")
 
 
 def file_metadata(instance, position):
-    """Aqui irá sua implementação"""
+    try:
+        data = instance.search(position)
+        print(data)
+    except IndexError:
+        sys.stderr.write("Posição inválida")
--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -1,15 +1,22 @@
 class Queue:
     def __init__(self):
         """Inicialize sua estrutura aqui"""
+        self.list = list()
 
     def __len__(self):
         """Aqui irá sua implementação"""
+        return len(self.list)
 
     def enqueue(self, value):
         """Aqui irá sua implementação"""
+        return self.list.append(value)
 
     def dequeue(self):
         """Aqui irá sua implementação"""
+        return self.list.pop(0)
 
     def search(self, index):
         """Aqui irá sua implementação"""
+        if index < 0 or index > (self.__len__() - 1):
+            raise IndexError
+        return self.list[index]
