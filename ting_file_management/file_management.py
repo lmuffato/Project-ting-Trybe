587a9120dce1edb11cbb737a85f609294f957b3b
lmuffato <lmiremuffato@gmail.com>
2022-02-20 21:02:45 -0300

    requisito 2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,26 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    # Se o arquivo tiver a extensão txt.
+    if not path_file.endswith('txt'):
+        # retorna o erro no terminal
+        return sys.stderr.write('Formato inválido\n')
+    try:
+        # Abrir o arquivo no modo leitura
+        with open(path_file, mode="r") as file:
+            # abre o arquivo, retornando um array onde cada elemento
+            # é uma linha separada no split('\n')
+            file_data = file.read().split('\n')
+            return file_data
+    # Se acontecer o a exception FileNotFoundError
+    except FileNotFoundError:
+        # retorna no terminal a mensagem
+        return sys.stderr.write(f'Arquivo {path_file} não encontrado\n')
+
+
+# teste automatizado
+# python3 -m pytest tests/test_file_mangement.py
+
+# teste manual
+# print(txt_importer("statics/arquivo_teste.txt"))
